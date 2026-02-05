37d77332cfa4cbc8ea67a0c471735b576c25280d
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-05 22:05:52 +0530

    variables in js

--- variables/variable.js
+++ variables/variable.js
@@ -1,24 +1,38 @@
-// variables in javascript 
+// // variables in javascript 
 
-let a=10;
-console.log(a);
+// let a=10;
+// console.log(a);
   
-// 1.scope 
-if (true) {
-    var msg="Hi im venkat";
-}
-console.log(msg);
+// // 1.scope 
+// if (true) {
+//     var msg="Hi im venkat";
+// }
+// console.log(msg);
 
-//let 
-if (true) {
-    let value="venkat"
-    console.log(value);
+// //let 
+// if (true) {
+//     let value="venkat"
+//     console.log(value);
     
-}
+// }
 
-// const 
-if (true) {
-    const name="hello welcome to javascript"
-    console.log(name);
+// // const 
+// if (true) {
+//     const name="hello welcome to javascript"
+//     console.log(name);
     
+// }
+
+
+const btn=document.querySelector("#btn");
+const head=document.querySelector("#heading");
+
+btn.addEventListener("click",getfun);
+
+let count=1
+
+function getfun() {
+  btn.textContent="Again Click";
+  head.textContent=`${count} clicks so far`
+  count=count+1
 }
