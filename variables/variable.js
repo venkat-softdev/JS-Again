ce428d476daebdf8abb45fc7682c79d1f2b80978
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-05 21:45:29 +0530

    variables in js

--- variables/variable.js
+++ variables/variable.js
@@ -3,3 +3,22 @@
 let a=10;
 console.log(a);
   
+// 1.scope 
+if (true) {
+    var msg="Hi im venkat";
+}
+console.log(msg);
+
+//let 
+if (true) {
+    let value="venkat"
+    console.log(value);
+    
+}
+
+// const 
+if (true) {
+    const name="hello welcome to javascript"
+    console.log(name);
+    
+}
