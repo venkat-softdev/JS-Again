2d7cb06eaeb474af93b5c3bf50e47ae9d6c87a42
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-06 20:37:22 +0530

    datatype in js

---
+++ datatypes/numdatatype.js
@@ -0,0 +1,20 @@
+let a=25;
+console.log(typeof a);
+
+// string type
+let name="venkat";
+console.log(typeof name);
+
+// boolean type
+let ismarried=true;
+console.log(typeof ismarried);
+
+// null type
+let phone=null;
+console.log("type of phone :",typeof phone);
+
+// undefined type
+
+let age;
+console.log("type of age :", typeof age);
+
