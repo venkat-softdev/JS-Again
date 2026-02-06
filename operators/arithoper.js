6c18bfc7494b185928c3383e03c50c656d60f304
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-06 21:21:41 +0530

    arithopertor in js

---
+++ operators/arithoper.js
@@ -0,0 +1,42 @@
+// arithmetic operator in javascript 
+
+let a= 10;
+let b=20;
+let c=a+b;
+console.log("The sum is :",c);
+
+// Ex:2
+let x=10;
+let y=3;
+
+let sum=x+y;
+console.log(sum);
+
+let difference=x-y;
+console.log(difference);
+
+let product=x*y;
+console.log(product);
+
+let quotient=x/y;
+console.log(quotient);
+
+let remainder=x%y;
+console.log(remainder);
+
+
+
+
+// increment and decrement 
+
+let count=5;
+count++
+console.log(count);
+count--;
+console.log(count);
+
+
+
+
+
+
