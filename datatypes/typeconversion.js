51a88c13d9cd8504b3ea36bd63a1242fd59c0e60
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-06 21:05:24 +0530

    type conversion in js

--- datatypes/typeconversion.js
+++ datatypes/typeconversion.js
@@ -1,3 +1,18 @@
 // type conversion in javascript 
 let a=25;
 console.log(a,typeof a);
+
+// ex:2
+
+let b=26;
+b=String(b);
+console.log(b,typeof b);
+
+
+
+// string to number 
+a="130";
+console.log(a,typeof a);
+a=Number(a);
+console.log(a,typeof a);
+
