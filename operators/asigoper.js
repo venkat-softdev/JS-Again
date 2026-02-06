3b8f69bda7b2adc2d7a5f4837435ae23225b023e
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-06 21:34:49 +0530

    asignoperator in js

--- operators/asigoper.js
+++ operators/asigoper.js
@@ -2,3 +2,24 @@
 let a=10;
 a=a+5;
 console.log(a);
+
+// Ex:2
+let x=2;
+const y=3;
+
+console.log(x);
+
+console.log((x=y+1));
+
+console.log((x=x*y));
+
+// Ex:3
+
+a=5;
+let b=10;
+let c=25;
+
+console.log((a=b));
+console.log((a=b=c));
+
+
