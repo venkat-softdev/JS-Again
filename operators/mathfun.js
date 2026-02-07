56512b2aec0e21a3d5b1f6c80cbb5eecbdf71422
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-07 13:58:27 +0530

    mathfunctions in js

--- operators/mathfun.js
+++ operators/mathfun.js
@@ -1,1 +1,16 @@
 // math functions in javascript 
+
+// pi value
+let a=Math.PI;
+console.log(a);
+
+// round
+let b=23.389;
+b=Math.round(b);
+console.log("round :",b);
+
+// floor
+
+let c=10.568;
+c=Math.floor(c);
+console.log("floor :",c);
