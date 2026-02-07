ea1dd1dfb5713526d102d55c79a4d44a8cdc34dd
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-07 14:04:09 +0530

    math functions in js

--- operators/mathfun.js
+++ operators/mathfun.js
@@ -14,3 +14,25 @@ console.log("round :",b);
 let c=10.568;
 c=Math.floor(c);
 console.log("floor :",c);
+
+// ceil
+let d=4.67;
+d=Math.ceil(d);
+console.log("ceil :",d);
+
+// sqrt
+
+let e=9;
+e=Math.sqrt(e);
+console.log("sqrt :",e);
+
+// power
+
+let f;
+f=Math.pow(2,4);
+console.log("power :",f);
+
+// max
+let max;
+max=Math.max(10,20,30,40,50,80);
+console.log("max value :",max);
