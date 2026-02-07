520bce2336a91db04956ac8fd401fe8a5cf093c4
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-07 14:20:49 +0530

    comparison operator in js

---
+++ operators/compareoper.js
@@ -0,0 +1,15 @@
+// comparison operator in javascript 
+
+let a=20;
+let b=30;
+console.log(a==b);
+
+// ex:2
+a=10;
+b="10";
+console.log(a==b);
+
+// Ex:3
+let c=10;
+let d="10";
+console.log(c===d);
