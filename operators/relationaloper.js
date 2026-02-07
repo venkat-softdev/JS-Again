d1fc93ee228efa8e159999779a3c2b9334976688
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-07 15:14:23 +0530

    relational oper in js

---
+++ operators/relationaloper.js
@@ -0,0 +1,26 @@
+// relational opoerator in javascript 
+
+//greater than 
+// lesser than 
+// greater than or equal to
+// lesser than or equal too
+
+// Ex:1
+
+let a=10;
+let b=20;
+
+console.log("Greater :",a>b);
+console.log("Lesser :",a<b);
+console.log("greater than or equal :" ,a>=b);
+console.log("lesser than or equal :",a<=b);
+
+//Ex:2
+let age=20;
+if (age>=18) {
+    console.log("Eligble to vote");
+    
+} else {
+    console.log("not eligble to vote");
+    
+}
