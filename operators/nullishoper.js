fc98d71dc9273e39d78a48b8e052cb7d527664b9
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-08 21:45:50 +0530

    nullsih oper in js

--- operators/nullishoper.js
+++ operators/nullishoper.js
@@ -13,3 +13,43 @@ console.log(user);
 console.log(user.city);
 user.city??='salem'
 console.log(user);
+
+//Ex:3
+const name=null??"Anonymous";
+console.log(name);
+
+const count=0;
+const quantitynullish=count??42;
+console.log(quantitynullish);
+
+const quantityOR=count||42;
+console.log(quantityOR);
+
+
+//Ex:4
+let users;
+console.log(users??"guest");
+let data=null
+console.log(data??"data not available");
+
+//Ex:5
+const response={
+    score:0,
+    notes:""
+}
+
+const displayscore=response.score ?? "no score added";
+console.log(displayscore);
+
+const displaynotes=response.notes ?? "no notes";
+console.log(displaynotes);
+
+// Ex:6
+let firstname=null;
+let lastname=undefined;
+let nickname="supercoder"
+
+console.log(firstname??lastname??nickname??"Anonymous");
+
+
+
