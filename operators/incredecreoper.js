6e649e4f33efe1607aab809fe3bad2f30cf5fc7d
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-08 22:01:13 +0530

    increment decrement oper in js

--- operators/incredecreoper.js
+++ operators/incredecreoper.js
@@ -7,3 +7,15 @@ console.log(a);
 let b=5;
 b--;
 console.log(b);
+
+// postfix increment
+//prefix increment 
+
+let x=3;
+const y=x++;
+console.log("x :",x,"y :",y);
+
+
+let i=3;
+let j=++i;
+console.log("i :",i,"j :",j);
