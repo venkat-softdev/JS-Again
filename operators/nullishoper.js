c83cbbd66e700c919bcd8ab7e431d9d436821e4a
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-08 21:28:06 +0530

    nullish oper in js

--- operators/nullishoper.js
+++ operators/nullishoper.js
@@ -1,1 +1,15 @@
 // Nullish operator in js 
+
+const a=null??"no value";
+console.log(a);
+
+const b=null??45;
+console.log(b);
+
+
+//Ex:2
+const user={name:"venkat"};
+console.log(user);
+console.log(user.city);
+user.city??='salem'
+console.log(user);
