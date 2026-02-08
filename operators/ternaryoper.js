740d8aa434b1ac4940dba980fbeef40535e2870e
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-08 20:01:12 +0530

    ternary opera in js

--- operators/ternaryoper.js
+++ operators/ternaryoper.js
@@ -1,39 +1,44 @@
-// ternary operator in javascript 
-let age=20;
-let aligible=age>=18?"eligble to vote":"not eligble to note"
+// ternary operator in javascript
+let age = 20;
+let aligible = age >= 18 ? "eligble to vote" : "not eligble to note";
 console.log(aligible);
 
 // ex:2
 
- age=26;
- const beverage=age>=21?"Beer":"Juice";
- console.log(beverage);
- 
+age = 26;
+const beverage = age >= 21 ? "Beer" : "Juice";
+console.log(beverage);
+
 //Ex:3
-const greeting=(person)=>{
-    const name=person?person.name:"stranger"
-    return `venkat , ${name}`
-}
-console.log(greeting({name:"Alice"}));
+const greeting = (person) => {
+  const name = person ? person.name : "stranger";
+  return `venkat , ${name}`;
+};
+console.log(greeting({ name: "Alice" }));
 
 // ex:4
-let isloggedIn=true;
-let message=`status :${isloggedIn ?"online":"offline"}`;
+let isloggedIn = true;
+let message = `status :${isloggedIn ? "online" : "offline"}`;
 console.log(message);
 
 //Ex:5
 function sayhello() {
-    console.log('hello');
-    
+  console.log("hello");
 }
 function saygoodbye() {
-    console.log("Goodbye!");
-    
+  console.log("Goodbye!");
 }
-let isleaving=true;
-isleaving?saygoodbye():sayhello();
+let isleaving = true;
+isleaving ? saygoodbye() : sayhello();
 
 //Ex:6
-let score=95;
-let grade=(score>90)?"A Grade":(score >80)?"B Grade":(score >70)?"C Grade":"Fail"
+let score = 95;
+let grade =
+  score > 90
+    ? "A Grade"
+    : score > 80
+      ? "B Grade"
+      : score > 70
+        ? "C Grade"
+        : "Fail";
 console.log(grade);
