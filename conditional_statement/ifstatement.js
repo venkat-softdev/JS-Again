3b5977339553718c3cc85519ee2721410f09f085
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-10 05:26:24 +0530

    if statement in js

--- conditional_statement/ifstatement.js
+++ conditional_statement/ifstatement.js
@@ -7,3 +7,12 @@ if (age>=18) {
     console.log("eligible to vote");
     
 }
+//Ex:2
+let isRaining=true;
+if (isRaining) {
+    console.log("dont forget an umbrella");
+    
+} else {
+    console.log("it might be a nice day out!");
+    
+}
