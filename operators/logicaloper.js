1f4ddbf6e2929d64e3f3e5a450b57b5497730024
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-09 21:07:25 +0530

    logical oper in js

--- operators/logicaloper.js
+++ operators/logicaloper.js
@@ -1,50 +1,46 @@
-// logical operators in javacsript 
+// logical operators in javacsript
 
 // && logical and
 // || logical or
-// ! logical not 
+// ! logical not
 
 // Ex:1
-let mark=45;
-console.log(mark>=35 && mark<=100);
+let mark = 45;
+console.log(mark >= 35 && mark <= 100);
 
-let a=5;
-console.log(a==2 || a==5);
+let a = 5;
+console.log(a == 2 || a == 5);
 
 // ex:2
-let age=20;
-let hasId=true;
+let age = 20;
+let hasId = true;
 
-if (age>=18 && hasId) {
-    console.log("Allowed to enter");
-    
+if (age >= 18 && hasId) {
+  console.log("Allowed to enter");
 } else {
-    console.log("Not Allowed");
-    
+  console.log("Not Allowed");
 }
 
 // Ex:3
-let isweekend=false;
-let isHoliday=true;
+let isweekend = false;
+let isHoliday = true;
 
-if (isweekend||isHoliday) {
-    console.log("It's time for a break!");
-    
+if (isweekend || isHoliday) {
+  console.log("It's time for a break!");
 } else {
-    console.log("Back to work");
-    
+  console.log("Back to work");
 }
 
 // Ex:4;
-let username="";
-let defaultname=username||"Guest";
-console.log("Welcome,"+defaultname);
-console.log(10||20);
+let username = "";
+let defaultname = username || "Guest";
+console.log("Welcome," + defaultname);
+console.log(10 || 20);
 
 // Ex:5
-let isloggedin=false;
+let isloggedin = false;
 console.log(!isloggedin);
 
 // Ex::6
-let temprature=31;
-console.log(!(temprature>30));
+let temprature = 31;
+console.log(!(temprature > 30));
