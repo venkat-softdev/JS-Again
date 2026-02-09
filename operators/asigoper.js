09a0a3cdfdef8c441cc9299f4eaa3d66c35467b9
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-09 21:07:56 +0530

    asign oper in js

--- operators/asigoper.js
+++ operators/asigoper.js
@@ -1,25 +1,23 @@
-// asignment operator in javascript 
-let a=10;
-a=a+5;
+// asignment operator in javascript
+let a = 10;
+a = a + 5;
 console.log(a);
 
 // Ex:2
-let x=2;
-const y=3;
+let x = 2;
+const y = 3;
 
 console.log(x);
 
-console.log((x=y+1));
+console.log((x = y + 1));
 
-console.log((x=x*y));
+console.log((x = x * y));
 
 // Ex:3
 
-a=5;
-let b=10;
-let c=25;
+a = 5;
+let b = 10;
+let c = 25;
 
-console.log((a=b));
-console.log((a=b=c));
-
-
+console.log((a = b));
+console.log((a = b = c));
