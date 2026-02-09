ebcd506edf048afb53191e17f1a20e6ffb4533f7
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-10 05:29:14 +0530

    if statement in js

--- conditional_statement/ifstatement.js
+++ conditional_statement/ifstatement.js
@@ -16,3 +16,22 @@ if (isRaining) {
     console.log("it might be a nice day out!");
     
 }
+
+//Ex:3
+let score=75;
+if (score>=90) {
+    console.log("Grade A");
+    
+}
+else if (score>=80) {
+    console.log("Grade B");
+    
+}
+else if (score>=70) {
+    console.log("Grade C");
+    
+}
+else{
+    console.log("Grade F");
+    
+}
