ce80dedebe14113d6cf0ce457aa53f882a011a36
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-09 21:08:26 +0530

    arithmetic oper in js

--- operators/arithoper.js
+++ operators/arithoper.js
@@ -1,50 +1,42 @@
-// arithmetic operator in javascript 
+// arithmetic operator in javascript
 
-let a= 10;
-let b=20;
-let c=a+b;
-console.log("The sum is :",c);
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log("The sum is :", c);
 
 // Ex:2
-let x=10;
-let y=3;
+let x = 10;
+let y = 3;
 
-let sum=x+y;
+let sum = x + y;
 console.log(sum);
 
-let difference=x-y;
+let difference = x - y;
 console.log(difference);
 
-let product=x*y;
+let product = x * y;
 console.log(product);
 
-let quotient=x/y;
+let quotient = x / y;
 console.log(quotient);
 
-let remainder=x%y;
+let remainder = x % y;
 console.log(remainder);
 
+// increment and decrement
 
-
-
-// increment and decrement 
-
-let count=5;
-count++
+let count = 5;
+count++;
 console.log(count);
 count--;
 console.log(count);
 
-
 // Ex:3
-let result=2+3*4;
-let res=(2+3)*4
-console.log("result :",res);
-
-
+let result = 2 + 3 * 4;
+let res = (2 + 3) * 4;
+console.log("result :", res);
 
 //Ex:4
-let concat="Hello"+"World";
+let concat = "Hello" + "World";
 console.log(concat);
-
-
