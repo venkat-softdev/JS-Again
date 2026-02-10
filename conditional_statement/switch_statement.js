55f8a55a7979eb0359994b2596f3dcd74d713f7e
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-10 20:13:32 +0530

    switch statement in js

--- conditional_statement/switch_statement.js
+++ conditional_statement/switch_statement.js
@@ -1,6 +1,6 @@
 // switch statement in javascript 
 
-let num=2;
+let num=3;
 
 switch (num) {
     case 1:
@@ -21,3 +21,76 @@ switch (num) {
             
         break;
 }
+//Ex:2
+ let fruit="apple";
+ let message="";
+
+ switch (fruit) {
+    case "banana":
+        console.log("hey it's banana");
+        
+        break;
+          
+        case"grape":
+        console.log("hey it's grape");
+        break;
+
+        case "apple":
+            console.log("hey it's apple");
+            break;
+            
+        
+    default:
+        console.log("found another fruit");
+        
+        break;
+ };
+
+ //Ex:3
+ let day="Saturday";
+ let typeodDay=""
+
+ switch (day) {
+    case "monday":
+        console.log("It's Monday");
+        
+        break;
+
+  
+        case "Saturday":
+        case "sunday":
+            typeodDay="It's weekend"
+            break;
+            
+ 
+    default:
+        typeodDay="It's weekday"
+        break;
+ }
+ console.log(typeodDay);
+ 
+ //Ex:4
+
+ function getwheatherMessage(weather) {
+    
+    switch (weather) {
+        case "sunny":
+            return "Don't forget sunglasses"
+            
+            break;
+
+            case"rainy":
+            return "Take an Umbrella"
+            break;
+
+            case "snowy":
+                return "wear warm clothes"
+                break;
+    
+        default:
+            return "check the forcast"
+            break;
+    }
+ }
+ console.log(getwheatherMessage("rainy"));
+ 
