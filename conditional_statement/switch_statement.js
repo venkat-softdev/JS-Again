405c23056cbdd969666de8d77b1eb76e12d31668
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-10 19:53:07 +0530

    switch statement in js

---
+++ conditional_statement/switch_statement.js
@@ -0,0 +1,23 @@
+// switch statement in javascript 
+
+let num=2;
+
+switch (num) {
+    case 1:
+        console.log("one");
+        
+        break;
+         
+        case 2:
+            console.log("two");
+            break;
+         
+         case 3:
+            console.log("Three");
+            break;
+               
+        default:
+            console.log("invalid number");
+            
+        break;
+}
