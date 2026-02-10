32ef54f6fb26f5a811eefd0d20def8f87b0d1893
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-10 06:05:24 +0530

    else if statement in js

--- conditional_statement/elseif_statement.js
+++ conditional_statement/elseif_statement.js
@@ -25,3 +25,20 @@ else {
     console.log(number,"number is equal to zero");
     
 }
+
+//Ex:2
+let avg=75;
+if (avg>=90&&avg<=100) {
+    console.log("Grade A");
+    
+}else if (avg>=80 && avg<=89) {
+    console.log("Grade B");
+    
+}else if (avg>=70 && avg<=79) {
+    console.log("Grade C");
+    
+}
+else{
+    console.log("Grade D");
+    
+}
