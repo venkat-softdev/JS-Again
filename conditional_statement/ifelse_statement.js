191ce06445dc3384604c6185de4950fc3f978440
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-10 05:41:48 +0530

    ifelse statement in js

---
+++ conditional_statement/ifelse_statement.js
@@ -0,0 +1,30 @@
+// if else statement in javascript 
+//syntax:
+//if(condition){
+//.........
+//}else{
+    //......
+    //}
+
+    //Ex:1
+    let age=20;
+    if (age>=18) {
+        console.log("you are an adult.");
+        
+    }
+
+    //Ex:2
+     age=15;
+     if (age>=18) {
+        console.log("you are an adult");
+        
+     }
+     else{
+        console.log("you are in Minor");
+        
+     }
+     //Ex:3
+     age=15;
+     let isAdult=(age>=18)?"yes":"no";
+     console.log(isAdult);
+     
