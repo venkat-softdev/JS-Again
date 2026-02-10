a414b4e557160e72c02be39e91b6d6c71d85e358
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-10 05:51:18 +0530

    else if statement in  js

---
+++ conditional_statement/elseif_statement.js
@@ -0,0 +1,27 @@
+// else if statement in javascript 
+
+/*
+if(cond){
+....
+}else if(cond){
+....
+}else{
+    ....
+    }
+
+*/
+
+//ex:1
+let number=1;
+if (number<0) {
+    console.log(number,"number is negative number");
+    
+}else if (number>0) {
+ console.log(number,"number is positive number");
+    
+} 
+
+else {
+    console.log(number,"number is equal to zero");
+    
+}
