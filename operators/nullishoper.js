6eea515e034f32aee693225e91557660ffc0e971
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-11 22:56:44 +0530

    nullsih oper in js

--- operators/nullishoper.js
+++ operators/nullishoper.js
@@ -1,63 +1,58 @@
-// Nullish operator in js 
+// Nullish operator in js
 
-const a=null??"no value";
+const a = null ?? "no value";
 console.log(a);
 
-const b=null??45;
+const b = null ?? 45;
 console.log(b);
 
-
 //Ex:2
-const user={name:"venkat"};
+const user = { name: "venkat" };
 console.log(user);
 console.log(user.city);
-user.city??='salem'
+user.city ??= "salem";
 console.log(user);
 
 //Ex:3
-const name=null??"Anonymous";
+const name = null ?? "Anonymous";
 console.log(name);
 
-const count=0;
-const quantitynullish=count??42;
+const count = 0;
+const quantitynullish = count ?? 42;
 console.log(quantitynullish);
 
-const quantityOR=count||42;
+const quantityOR = count || 42;
 console.log(quantityOR);
-
 
 //Ex:4
 let users;
-console.log(users??"guest");
-let data=null
-console.log(data??"data not available");
+console.log(users ?? "guest");
+let data = null;
+console.log(data ?? "data not available");
 
 //Ex:5
-const response={
-    score:0,
-    notes:""
-}
+const response = {
+  score: 0,
+  notes: "",
+};
 
-const displayscore=response.score ?? "no score added";
+const displayscore = response.score ?? "no score added";
 console.log(displayscore);
 
-const displaynotes=response.notes ?? "no notes";
+const displaynotes = response.notes ?? "no notes";
 console.log(displaynotes);
 
 // Ex:6
-let firstname=null;
-let lastname=undefined;
-let nickname="supercoder"
+let firstname = null;
+let lastname = undefined;
+let nickname = "supercoder";
 
-console.log(firstname??lastname??nickname??"Anonymous");
-
-
+console.log(firstname ?? lastname ?? nickname ?? "Anonymous");
 
 // ex:7
-const usersettings={};
+const usersettings = {};
 
-usersettings.theme??="dark";
-usersettings.fontsize ??=16;
-usersettings.theme??="light";
+usersettings.theme ??= "dark";
+usersettings.fontsize ??= 16;
+usersettings.theme ??= "light";
 console.log(usersettings);
-
