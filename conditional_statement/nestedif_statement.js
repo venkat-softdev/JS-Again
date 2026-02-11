a84b0887720c105c8179f9f449428151374fcb00
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-11 22:55:41 +0530

    nestedif  statement in js

--- conditional_statement/nestedif_statement.js
+++ conditional_statement/nestedif_statement.js
@@ -7,91 +7,71 @@ if(cond){
 }
 */
 
-let english=95,tamil=98,maths=75;
-let total,avg;
+let english = 95,
+  tamil = 98,
+  maths = 75;
+let total, avg;
 
-total=english+tamil+maths;
-avg=total/3;
+total = english + tamil + maths;
+avg = total / 3;
 console.log(total);
 console.log(avg.toFixed(2));
 
-if (english>=35 && tamil>=35 && english>=35) {
-    console.log("result :pass");
-    
-    if (avg>90 &&avg<=100) {
-        console.log("A Grade");
-        
-    }else if (avg>80 && avg<=90) {
-        console.log("B Grade");
-        
-    }
-    else if (avg >70 && avg<=79) {
-        console.log("C Grade");
-        
-    }
-    else{
-        console.log("D Grade");
-        
-    }
-}
-else{
-    console.log("Result :Fail");
-    
+if (english >= 35 && tamil >= 35 && english >= 35) {
+  console.log("result :pass");
+
+  if (avg > 90 && avg <= 100) {
+    console.log("A Grade");
+  } else if (avg > 80 && avg <= 90) {
+    console.log("B Grade");
+  } else if (avg > 70 && avg <= 79) {
+    console.log("C Grade");
+  } else {
+    console.log("D Grade");
+  }
+} else {
+  console.log("Result :Fail");
 }
 
 // Ex:2
 
-let marks=85;
+let marks = 85;
 
-if (marks>=40) {
-    
-    if (marks>=80) {
-        console.log("Destinction");
-        
-    } else {
-        console.log("Passed");
-        
-    }
-
+if (marks >= 40) {
+  if (marks >= 80) {
+    console.log("Destinction");
+  } else {
+    console.log("Passed");
+  }
 } else {
-    console.log("Failed in Exam");
-    
+  console.log("Failed in Exam");
 }
 
 //Ex:3
 
-let age=25;
-let hasqualification=true;
+let age = 25;
+let hasqualification = true;
 
-if (age>=18) {
-    
-    if (hasqualification) {
-        console.log("You are eliegble to apply job");
-        
-    } else {
-        console.log("you need to gain the required qualification first");
-        
-    }
+if (age >= 18) {
+  if (hasqualification) {
+    console.log("You are eliegble to apply job");
+  } else {
+    console.log("you need to gain the required qualification first");
+  }
 } else {
-    console.log("you are not old enough to work");
-    
+  console.log("you are not old enough to work");
 }
 
 //Ex:4
-let username="admin";
-let password="correctpassword";
+let username = "admin";
+let password = "correctpassword";
 
-if (username==="admin") {
-    
-    if (password==="correctpassword") {
-        
-        console.log("Access granted .welcome,admin");
-        
-    } else {
-        console.log("Incorrect Password");
-        
-    }
+if (username === "admin") {
+  if (password === "correctpassword") {
+    console.log("Access granted .welcome,admin");
+  } else {
+    console.log("Incorrect Password");
+  }
 } else {
-    console.log("Username not found");
-    
-} 
+  console.log("Username not found");
+}
