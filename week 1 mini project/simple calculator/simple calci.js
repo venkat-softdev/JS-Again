18e18ff87fbc4bb2c394f4146c3ed64faa4c8904
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-14 11:32:01 +0530

    simple calculator in js

---
+++ week 1 mini project/simple calculator/simple calci.js
@@ -0,0 +1,26 @@
+// javascript starting 
+const button=document.querySelector("button");
+function calculate(op) {
+    
+    let n1=Number(document.querySelector("#num1").value);
+    let n2=Number(document.querySelector("#num2").value);
+    let result;
+
+    if (op==='+') {
+        result=n1+n2
+    }
+    else if(op==='-'){
+    
+        result=n1-n2
+    }
+    else if (op==='*') {
+        result=n1*n2
+    }
+    else if (op==='/') {
+        result=n1/n2
+    }
+
+    let output=document.querySelector("#output");
+    output.innertText="Reult :"+ result;
+    output.style.display="block"
+}
