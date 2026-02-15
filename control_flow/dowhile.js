b68cf2989b4a494d16ea9e2bdc33f1903a2360bc
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-15 23:55:00 +0530

    do while loop in js

---
+++ control_flow/dowhile.js
@@ -0,0 +1,18 @@
+// do while loop in javascript 
+
+/* 
+do{
+ // code to be executed if the condition is true
+}
+ while(condition)
+*/
+
+ let table=2;
+ let limit=10;
+ let i=1;
+
+ do {
+    console.log(table +" X "+i+"="+(table*i));
+    i++
+    
+ } while (i<=limit);
