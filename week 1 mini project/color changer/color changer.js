2d705e9b48e5ecad57b9bbfa730a9351be662b12
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-15 10:54:05 +0530

    background color changer

---
+++ week 1 mini project/color changer/color changer.js
@@ -0,0 +1,11 @@
+// javascript starting 
+
+const btn=document.querySelector("#btn");
+btn.addEventListener("click",change);
+
+function change() {
+    
+    let randomcolor="#"+Math.floor(Math.random()*16777215);
+     randomcolor.toString(16);
+     document.body.style.backgroundColor=randomcolor;
+}
