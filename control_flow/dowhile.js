0da501dd4d3a4bd6c82a1edad04254956647f19b
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-16 22:52:35 +0530

    do while in js

--- control_flow/dowhile.js
+++ control_flow/dowhile.js
@@ -16,3 +16,29 @@ do{
     i++
     
  } while (i<=limit);
+
+
+ // Ex:2
+
+ i=0;
+
+ do {
+   console.log("The Number is "+i);
+   i++;
+ } while (i<5);
+
+ //Ex:3
+
+ let x=10;
+ do {
+   console.log("This will run at least once .X is: "+x);
+   x++
+ } while (x<5);
+
+ //Ex:4
+
+ let num;
+
+ do {
+   num="please enter a number graeter than 100 :"
+ } while (condition);
