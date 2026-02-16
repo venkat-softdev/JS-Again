82f487ba211ae22cb9f3576affac9740ddc8ed6f
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-16 22:53:27 +0530

    while in js

--- control_flow/while.js
+++ control_flow/while.js
@@ -8,65 +8,58 @@ while(condition){
 
 // ex:1
 
-let i=1;
-while (i<=10) {
-    console.log(i);
-    i++
-    
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
 }
 
 // Ex:2
 
-let n=0;
-let x=0;
-while (n<3) {
-    n++;
-    x+=n
-};
-console.log("X value :",x);
-console.log("N value :",n);
-
+let n = 0;
+let x = 0;
+while (n < 3) {
+  n++;
+  x += n;
+}
+console.log("X value :", x);
+console.log("N value :", n);
 
 // Ex:3
-i=0;
-while (i<5) {
-    console.log(i);
- i++
-    
+i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
 }
 
 // ex:4
 
-const fruits=['Apple','Orange','Mango'];
-let index=0;
+const fruits = ["Apple", "Orange", "Mango"];
+let index = 0;
 
-while (index<fruits.length) {
-    console.log("I like "+fruits[index]);
-    index++
-    
+while (index < fruits.length) {
+  console.log("I like " + fruits[index]);
+  index++;
 }
 
 // Ex:5
 
-let inbox=["Email 1","Email 2","Email 3"];
+let inbox = ["Email 1", "Email 2", "Email 3"];
 
-while (inbox.length>0) {
-    
-    let currentmail=inbox.shift();
-    console.log("Deleting :"+currentmail);
-    
+while (inbox.length > 0) {
+  let currentmail = inbox.shift();
+  console.log("Deleting :" + currentmail);
 }
 
 // ex:6
 
-let count=0;
+let count = 0;
 
-while (count<10) {
-    count++
+while (count < 10) {
+  count++;
 
-    if (count % 2==0) {
-        continue;
-    }
-    console.log(count+" count is a odd number");
-    
+  if (count % 2 == 0) {
+    continue;
+  }
+  console.log(count + " count is a odd number");
 }
