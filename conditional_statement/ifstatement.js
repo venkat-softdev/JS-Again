9bcc2793955b407aed925b5a26702f6ae743d760
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-16 22:53:45 +0530

    if statement in js

--- conditional_statement/ifstatement.js
+++ conditional_statement/ifstatement.js
@@ -2,36 +2,26 @@
 
 //Ex:1
 
-let age=20;
-if (age>=18) {
-    console.log("eligible to vote");
-    
+let age = 20;
+if (age >= 18) {
+  console.log("eligible to vote");
 }
 //Ex:2
-let isRaining=true;
+let isRaining = true;
 if (isRaining) {
-    console.log("dont forget an umbrella");
-    
+  console.log("dont forget an umbrella");
 } else {
-    console.log("it might be a nice day out!");
-    
+  console.log("it might be a nice day out!");
 }
 
 //Ex:3
-let score=75;
-if (score>=90) {
-    console.log("Grade A"); 
-    
-}
-else if (score>=80) {
-    console.log("Grade B");
-    
-}
-else if (score>=70) {
-    console.log("Grade C");
-    
-}
-else{
-    console.log("Grade F");
-    
+let score = 75;
+if (score >= 90) {
+  console.log("Grade A");
+} else if (score >= 80) {
+  console.log("Grade B");
+} else if (score >= 70) {
+  console.log("Grade C");
+} else {
+  console.log("Grade F");
 }
