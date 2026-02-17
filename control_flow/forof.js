a5485fb4677ca7044429124f091d1e9d83b402cf
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-17 22:32:50 +0530

    for of loop in js

--- control_flow/forof.js
+++ control_flow/forof.js
@@ -6,3 +6,27 @@ for (const name of names) {
     console.log(name);
     
 }
+
+// Ex:2
+
+const array=['a','b','c'];
+for (const values of array) {
+    console.log(values);
+    
+}
+
+//Ex:3
+
+const iterable=[10,20,30];
+for (let values of  iterable) {
+    values += 1;
+    console.log(values);
+    
+}
+
+//Ex:4
+let value='boo';
+for (const result of value) {
+    console.log(result);
+    
+}
