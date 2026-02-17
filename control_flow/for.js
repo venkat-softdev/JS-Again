8cece32b428fe9770ac1ef4e020400beb72bf039
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-17 21:50:16 +0530

    for loop in js

--- control_flow/for.js
+++ control_flow/for.js
@@ -6,3 +6,20 @@ for (let i = 1; i <= 10; i++) {
     
     
 }
+
+// Ex:2
+
+let arr=[];
+for (let i = 0; i <= 100; i++) {
+    arr.push(i)
+    
+}
+console.log(arr);
+
+//Ex:3
+const fruits=['apple','banana','cheery'];
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+    
+    
+}
