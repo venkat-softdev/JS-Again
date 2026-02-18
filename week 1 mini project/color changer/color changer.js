733d3114d48e5877b58aab5dbb3d6bf56ae360ab
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-18 22:20:46 +0530

    color changer in js

--- week 1 mini project/color changer/color changer.js
+++ week 1 mini project/color changer/color changer.js
@@ -1,11 +1,10 @@
-// javascript starting 
+// javascript starting
 
-const btn=document.querySelector("#btn");
-btn.addEventListener("click",change);
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", change);
 
 function change() {
-    
-    let randomcolor="#"+Math.floor(Math.random()*16777215);
-     randomcolor.toString(16);
-     document.body.style.backgroundColor=randomcolor;
+  let randomcolor = "#" + Math.floor(Math.random() * 16777215);
+  randomcolor.toString(16);
+  document.body.style.backgroundColor = randomcolor;
 }
