80d743e2583fe2abddf5c3745bae297b8fcc5508
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-18 22:21:13 +0530

    login validation in js

--- week 1 mini project/login validation/login validation.js
+++ week 1 mini project/login validation/login validation.js
@@ -1,24 +1,21 @@
-// javascript starting 
+// javascript starting
 
-const btn=document.querySelector("#btn");
-btn.addEventListener("click",logfun);
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", logfun);
 
 function logfun() {
-    
-    let username=document.querySelector("#user").value;
-    let pass=document.querySelector("#pass").value;
-    let msg=document.querySelector("#msg")
+  let username = document.querySelector("#user").value;
+  let pass = document.querySelector("#pass").value;
+  let msg = document.querySelector("#msg");
 
-    if (username=="") {
-        msg.textContent="Username required"
-        msg.style.display="block"
-    }
-    else if (pass.length < 6) {
-        msg.textContent="Password Must be 6 Digits"
-        msg.style.display="block"
-    }
-    else {
-        msg.textContent="Login Success"
-        msg.style.display="block"
-    }
+  if (username == "") {
+    msg.textContent = "Username required";
+    msg.style.display = "block";
+  } else if (pass.length < 6) {
+    msg.textContent = "Password Must be 6 Digits";
+    msg.style.display = "block";
+  } else {
+    msg.textContent = "Login Success";
+    msg.style.display = "block";
+  }
 }
