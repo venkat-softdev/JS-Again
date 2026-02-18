a65d6a7259c88435d16f08d606e3f4ef0dcced4c
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-18 21:40:08 +0530

    for in loop in js

---
+++ control_flow/forin.js
@@ -0,0 +1,14 @@
+// for in loop javavscript 
+
+let user={
+    name:"venkat",
+    age:35,
+    city:"vadadlur"
+}
+
+for (const prop in user) {
+   console.log(prop+":"+user[prop]);
+   
+    
+    
+}
