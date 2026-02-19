763542ef119722b04b972a516bbd2d0fac760251
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-19 21:54:40 +0530

    relational oper in js

--- operators/relationaloper.js
+++ operators/relationaloper.js
@@ -1,35 +1,33 @@
-// relational opoerator in javascript 
+// relational opoerator in javascript
 
-//greater than 
-// lesser than 
+//greater than
+// lesser than
 // greater than or equal to
 // lesser than or equal too
 
 // Ex:1
 
-let a=10;
-let b=20;
+let a = 10;
+let b = 20;
 
-console.log("Greater :",a>b);
-console.log("Lesser :",a<b);
-console.log("greater than or equal :" ,a>=b);
-console.log("lesser than or equal :",a<=b);
+console.log("Greater :", a > b);
+console.log("Lesser :", a < b);
+console.log("greater than or equal :", a >= b);
+console.log("lesser than or equal :", a <= b);
 
 //Ex:2
-let age=20;
-if (age>=18) {
-    console.log("Eligble to vote");
-    
+let age = 20;
+if (age >= 18) {
+  console.log("Eligble to vote");
 } else {
-    console.log("not eligble to vote");
-    
+  console.log("not eligble to vote");
 }
 
 // Ex:3
-const car={
-    make:"Honda",
-    model:"Accord",
-    year:1995,
-}
-console.log('model' in car);
-console.log('color' in car);
+const car = {
+  make: "Honda",
+  model: "Accord",
+  year: 1995,
+};
+console.log("model" in car);
+console.log("color" in car);
