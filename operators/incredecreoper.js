71aa71a22cac3b9a5a5044e27d3fd09be0cab8c2
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-19 21:55:20 +0530

    increment oper in js

--- operators/incredecreoper.js
+++ operators/incredecreoper.js
@@ -1,21 +1,20 @@
 // increment and decrement operator in javascript
 
-let a=1;
+let a = 1;
 a++;
 console.log(a);
 
-let b=5;
+let b = 5;
 b--;
 console.log(b);
 
 // postfix increment
-//prefix increment 
+//prefix increment
 
-let x=3;
-const y=x++;
-console.log("x :",x,"y :",y);
+let x = 3;
+const y = x++;
+console.log("x :", x, "y :", y);
 
-
-let i=3;
-let j=++i;
-console.log("i :",i,"j :",j);
+let i = 3;
+let j = ++i;
+console.log("i :", i, "j :", j);
