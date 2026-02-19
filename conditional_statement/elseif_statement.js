d44dafa5bf9406d2a3148fc6a9e8316a04c8090e
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-19 21:48:15 +0530

    elseif in js

--- conditional_statement/elseif_statement.js
+++ conditional_statement/elseif_statement.js
@@ -1,4 +1,4 @@
-// else if statement in javascript 
+// else if statement in javascript
 
 /*
 if(cond){
@@ -12,49 +12,36 @@ if(cond){
 */
 
 //ex:1
-let number=1;
-if (number<0) {
-    console.log(number,"number is negative number");
-    
-}else if (number>0) {
- console.log(number,"number is positive number");
-    
-} 
+let number = 1;
+if (number < 0) {
+  console.log(number, "number is negative number");
+} else if (number > 0) {
+  console.log(number, "number is positive number");
+} else {
+  console.log(number, "number is equal to zero");
+}
 
-else {
-    console.log(number,"number is equal to zero");
-    
-}
-  
 //Ex:2
-let avg=75;
-if (avg>=90&&avg<=100) {
-    console.log("Grade A");
-    
-}else if (avg>=80 && avg<=89) {
-    console.log("Grade B");
-    
-}else if (avg>=70 && avg<=79) {
-    console.log("Grade C");
-    
-}
-else{
-    console.log("Grade D");
-    
+let avg = 75;
+if (avg >= 90 && avg <= 100) {
+  console.log("Grade A");
+} else if (avg >= 80 && avg <= 89) {
+  console.log("Grade B");
+} else if (avg >= 70 && avg <= 79) {
+  console.log("Grade C");
+} else {
+  console.log("Grade D");
 }
 
 //Ex:3
-let time=new Date().getHours();
+let time = new Date().getHours();
 let greeting;
-if (time<10) {
-    greeting="Good morning"
-}
-else if (time<20) {
-    greeting="good day"
-}
-else{
-    greeting="good evening"
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "good day";
+} else {
+  greeting = "good evening";
 }
 
 console.log(greeting);
-
