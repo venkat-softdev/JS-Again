f8b7dfb34b368d3babc9dff6fb9c7cc620a9e984
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-21 20:25:17 +0530

    continue statement in js

--- continue.js
+++ continue.js
@@ -10,3 +10,15 @@ for (let i = 0; i < 10; i++) {
     console.log(i);
     
 }
+console.log('next');
+
+// Ex:2
+for (let i = 0; i <=10; i++) {
+ 
+    if (i%2==0) {
+        continue;
+    }
+    console.log(i);
+    
+    
+}
