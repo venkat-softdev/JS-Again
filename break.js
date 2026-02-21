68a670876a16308fb625f20a86bcf467fb61731d
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-21 20:17:41 +0530

    break statement in js

--- break.js
+++ break.js
@@ -9,3 +9,62 @@ for (let i = 0; i <10; i++) {
     }
     
 }
+
+//Ex:2
+for (let i = 0; i < 10; i++) {
+    
+    if (i==4) {
+        break;
+    }
+    console.log("the Number is "+i);
+    
+}
+// ex:3
+
+let i=0;
+while (i<5) {
+    if (i===3) {
+        break
+    }
+    console.log(i);
+    i++
+}
+
+//Ex:4
+let fruits='appl';
+switch (fruits) {
+    case 'orange':
+        console.log('hey its orange');
+        
+        break;
+      
+        case 'mango':
+            console.log('hey its mango');
+            break;
+            
+            case 'apple':
+                console.log('hey its apple');
+                break;
+
+                
+
+    default:
+        console.log('its another fruit');
+        
+        break;
+}
+
+//Ex:5
+outerloop: for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        
+        if (i===1 && j==1) {
+            break outerloop;
+        }
+    
+        console.log(`i:${i},j:${j}`);
+        
+    }
+
+    
+}
