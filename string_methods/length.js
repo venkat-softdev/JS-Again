8928b1fa00ec7d635ce21e4945b58a6e91a03197
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-22 23:59:59 +0530

    length in js

--- string_methods/length.js
+++ string_methods/length.js
@@ -9,3 +9,17 @@ let fruits=['apple','aorange','banana'];
 console.log(fruits.length);
 console.log(fruits[0].length,fruits[0]);
 
+
+//Ex:3 Dynamic change
+
+let arr=[1,2,3,4,5]
+arr.length=3;
+console.log(arr);
+
+//ex:4 
+let nums=[10,20,30];
+for (let i = 0; i < nums.length; i++) {
+   console.log(nums[i]);
+   
+    
+}
