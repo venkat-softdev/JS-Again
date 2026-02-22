9e2cb0671a0ab2ce5a195e69192df39f8162b63c
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-22 23:22:00 +0530

    append in js

---
+++ string_methods/append.js
@@ -0,0 +1,18 @@
+// append in jaavavscript methods
+
+//Ex:1
+let fruits=['apple','lemon'];
+console.log(fruits);
+
+fruits+=', orange'
+console.log("append :",fruits);
+
+//ex:2
+let str='Hi ';
+str+=' Bro';
+console.log(str);
+
+//Ex:3
+let msg='Hello';
+msg+=' Venkat';
+console.log(msg);
