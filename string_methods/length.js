79074c8f6a8440a842577494b76f7f9119c2c5df
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-22 23:56:30 +0530

    length in js

---
+++ string_methods/length.js
@@ -0,0 +1,11 @@
+// length in javavscript 
+
+let names='venkat';
+console.log(names.length);
+
+//Ex:2 arry length
+
+let fruits=['apple','aorange','banana'];
+console.log(fruits.length);
+console.log(fruits[0].length,fruits[0]);
+
