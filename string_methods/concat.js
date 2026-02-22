813559b676ecec1c63b647777a04cab570bcd911
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-22 23:01:24 +0530

    concat in js

---
+++ string_methods/concat.js
@@ -0,0 +1,17 @@
+// concat in javascript string methods
+
+//Ex:1
+const str1="venkat";
+const str2="vp";
+
+const newstr=str1.concat(" ",str2);
+console.log(newstr);
+
+//Ex:2
+const letters=['a','b','c'];
+const numbers=[1,2,3,];
+
+const combined=letters.concat(numbers)
+console.log(combined);
+
+//Ex:3
