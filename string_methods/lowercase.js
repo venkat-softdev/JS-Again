01222e73aaa1913d1cec6e63a73e62c38569ecba
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-23 22:33:18 +0530

    tolowercase in js

--- string_methods/lowercase.js
+++ string_methods/lowercase.js
@@ -6,3 +6,16 @@ let lowerstring=mixedcasestring.toLowerCase();
 console.log(lowerstring);
 
 //ex:2
+const data='USER_123! @HOME';
+console.log(data.toLowerCase());
+
+//ex:3
+const input='Admin';
+if (input.toLowerCase()==='admin') {
+    console.log('Access Graunted');
+    
+}
+else{
+    console.log('not Access');
+    
+}
