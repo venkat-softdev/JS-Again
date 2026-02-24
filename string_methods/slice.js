187e0dd134389068c6c4fe649abe08e0b3bc44c0
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-24 20:55:01 +0530

    slice in js

--- string_methods/slice.js
+++ string_methods/slice.js
@@ -6,3 +6,14 @@ const tropical=fruits.slice(2)
 console.log(tropical);
 
 //ex:2
+
+const citrus=fruits.slice(1,3);
+console.log(citrus);
+
+//ex:3
+const lasttwo=fruits.slice(-2);
+console.log(lasttwo);
+
+// ex:4
+const middleslice=fruits.slice(-4,-1);
+console.log(middleslice);
