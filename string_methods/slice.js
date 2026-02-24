49853bc3079bd8ff79e8e7f1b29bf6a642876d7d
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-24 21:02:57 +0530

    slice in js

--- string_methods/slice.js
+++ string_methods/slice.js
@@ -17,3 +17,13 @@ console.log(lasttwo);
 // ex:4
 const middleslice=fruits.slice(-4,-1);
 console.log(middleslice);
+
+//Ex:5
+let greeting='hello world,hello again';
+let newstring=greeting.replace('hello','hi');
+console.log(newstring);
+
+//Ex:6
+let msg='The color of the sky is  blue.the color of a flower is also blue';
+let newmsg=msg.replaceAll('color','colour')
+console.log(newmsg);
