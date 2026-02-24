636d5644e7338ca021399b3c811c9e5528b2e5f9
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-24 19:27:30 +0530

    indexof in js

--- string_methods/indexof.js
+++ string_methods/indexof.js
@@ -5,3 +5,21 @@
  let result=firstname.indexOf('a');
  console.log(result);
  
+
+ //ex:2
+ let text="Hello World";
+ let position=text.indexOf('World');
+ console.log(position);
+ 
+ //ex:3
+
+ text='hello world';
+ position=text.indexOf('javavascript');
+ console.log(position);
+ 
+ //ex:4
+ const greeting='Hello World';
+ const position1=greeting.indexOf('World');
+ console.log(position1);
+ 
+ 
