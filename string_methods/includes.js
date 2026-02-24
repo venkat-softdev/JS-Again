6ff8793534e192d4e0487f78f2b4352ddbb50c35
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-24 21:13:47 +0530

    includes in js

---
+++ string_methods/includes.js
@@ -0,0 +1,17 @@
+// includes in javascript 
+let pets=['cat','dog','bat'];
+let check=pets.includes('bat')
+console.log(check);
+
+//ex:2
+const numbers=[10,20,30,40,50];
+console.log(numbers.includes(20,3));;
+
+
+//ex:3
+const colors=['red','blue','green','yellow'];
+console.log(colors.includes('blue',4));
+
+//ex:4
+const sentence='javavascript is awesome';
+console.log(sentence.includes('is'));
