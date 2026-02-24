1057c95b42beb893e4f34185ef3fe8bc93d9769b
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-24 20:20:41 +0530

    lastindex.js

---
+++ string_methods/lastindexof.js
@@ -0,0 +1,19 @@
+// lastindexof in javascript 
+
+const fname='venkatvp';
+let result=fname.lastIndexOf('v');
+console.log(result);
+
+//Ex:2
+const msg='Hey sam, sam are you coming to the meeting sam.';
+const lastsam=msg.lastIndexOf('sam');
+console.log(lastsam);
+
+//ex:3
+const str='I love javascript';
+const value=str.lastIndexOf('python');
+console.log(value);
+
+//ex:4
+const animals=['dodo','tiger','penguin','dodo'];
+console.log(animals.lastIndexOf('dodo'));
