0e9e2683813064842ce67a237965bea64cfc0c56
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-25 20:32:17 +0530

    template literals in js

---
+++ string_methods/template_literals.js
@@ -0,0 +1,11 @@
+// template literals in javavscript 
+
+//ex:1
+
+const name='venkat';
+console.log(`hello ${name}`);
+
+//Ex:2
+const item='Book';
+const template=`<li>${item}</li>`
+console.log(template);
