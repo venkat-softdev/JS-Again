c1206bcc03853f2caa1cdf6c55dd59560a48b73d
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-26 22:46:28 +0530

    padst and padend in js

--- string_methods/padst_padend.js
+++ string_methods/padst_padend.js
@@ -1,38 +1,36 @@
-// padstart and padend in javacsript 
+// padstart and padend in javacsript
 
-let a='5';
-a=a.padStart(4,0);
+let a = "5";
+a = a.padStart(4, 0);
 console.log(a);
 
-let b='5';
-b=b.padEnd(4,0);
+let b = "5";
+b = b.padEnd(4, 0);
 console.log(b);
 
 //ex:2
 
-const str='abc';
+const str = "abc";
 console.log(str.padStart(10));
 
 //ex:3
-let num='5';
-let padnum=String(num).padStart(5,'0');
+let num = "5";
+let padnum = String(num).padStart(5, "0");
 console.log(padnum);
 
 //ex:4
-num='123';
-padnum=String(num).padStart(6,'0');
+num = "123";
+padnum = String(num).padStart(6, "0");
 console.log(padnum);
 
 //Ex:5
-const str2='abc';
-console.log(str2.padStart(10,'foo'));
+const str2 = "abc";
+console.log(str2.padStart(10, "foo"));
 
 //ex:6
-const str3='Hi';
-console.log(str3.padEnd(10,'!'));
+const str3 = "Hi";
+console.log(str3.padEnd(10, "!"));
 
 //Ex:7
-const str4='code';
-console.log(str4.padEnd(10,'javascript'));
-
-
+const str4 = "code";
+console.log(str4.padEnd(10, "javascript"));
