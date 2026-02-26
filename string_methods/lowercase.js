13f2507823440a6c7664be9113e538beb491b6ce
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-26 22:46:00 +0530

    tolowercase in js

--- string_methods/lowercase.js
+++ string_methods/lowercase.js
@@ -1,29 +1,25 @@
-// lowercase in javavscript string methods 
+// lowercase in javavscript string methods
 
 //ex:1
-let mixedcasestring='HELLO WORLD';
-let lowerstring=mixedcasestring.toLowerCase();
+let mixedcasestring = "HELLO WORLD";
+let lowerstring = mixedcasestring.toLowerCase();
 console.log(lowerstring);
 
 //ex:2
-const data='USER_123! @HOME';
+const data = "USER_123! @HOME";
 console.log(data.toLowerCase());
 
 //ex:3
-const input='Admin';
-if (input.toLowerCase()==='admin') {
-    console.log('Access Graunted');
-    
-}
-else{
-    console.log('not Access');
-    
+const input = "Admin";
+if (input.toLowerCase() === "admin") {
+  console.log("Access Graunted");
+} else {
+  console.log("not Access");
 }
 
 //Ex;4
-const tags=['javascript','HTML','CSS'];
-const lowertags=tags.map((tag)=>{
-    return tag.toLowerCase()
-})
+const tags = ["javascript", "HTML", "CSS"];
+const lowertags = tags.map((tag) => {
+  return tag.toLowerCase();
+});
 console.log(lowertags);
- 
