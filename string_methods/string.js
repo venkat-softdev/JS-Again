6e96610ba37da39e497d301f9a78c3bd76ebce57
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-26 22:46:55 +0530

    string in js

--- string_methods/string.js
+++ string_methods/string.js
@@ -1,7 +1,7 @@
 // string method in javascript
 // concatenation
 
-let firstname="venkat";
-let lastname="vp";
-let result=firstname+" "+lastname;
+let firstname = "venkat";
+let lastname = "vp";
+let result = firstname + " " + lastname;
 console.log(result);
