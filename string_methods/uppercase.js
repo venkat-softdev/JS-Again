5993d8a2d213d7242e760baf687f071969acd065
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-26 22:43:05 +0530

    to uppercase in js

--- string_methods/uppercase.js
+++ string_methods/uppercase.js
@@ -1,22 +1,22 @@
 // uppercase in javascript string methods
 
 //Ex:1
-let originalstring='Hello - World';
-let uppercasestring=originalstring.toUpperCase();
+let originalstring = "Hello - World";
+let uppercasestring = originalstring.toUpperCase();
 console.log(uppercasestring);
 
 //Ex:2
-let greeting='good morning';
-let loudgreeting=greeting.toUpperCase();
+let greeting = "good morning";
+let loudgreeting = greeting.toUpperCase();
 console.log(loudgreeting);
 
 //Ex:3
-let result='convert this'.toUpperCase();
+let result = "convert this".toUpperCase();
 console.log(result);
 
 //Ex:4
-let data=['javavscript' ,'html','css'];
-result=data.map((item)=>{
-    return item.toUpperCase()
-})
+let data = ["javavscript", "html", "css"];
+result = data.map((item) => {
+  return item.toUpperCase();
+});
 console.log(result);
