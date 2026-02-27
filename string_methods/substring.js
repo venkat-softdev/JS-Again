7f93aad18a74d16823c10af1823a1bf69add629e
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-27 21:38:28 +0530

    substring in  js

--- string_methods/substring.js
+++ string_methods/substring.js
@@ -1,23 +1,21 @@
-// substring  in javavscript 
+// substring  in javavscript
 
-let text='Hello world';
-let result=text.substring(0,5);
+let text = "Hello world";
+let result = text.substring(0, 5);
 console.log(result);
 
 //ex:2
 
-text='hello world';
-result=text.substring(6);
+text = "hello world";
+result = text.substring(6);
 console.log(result);
 
 //Ex;3
-text='Hello world';
-result=text.substring(-3,5);
+text = "Hello world";
+result = text.substring(-3, 5);
 console.log(result);
 
 //ex:4
-let longtext='the concept of immutability is cruicial when you are working';
-let truncatedtext=longtext.substring(0,20)+"...";
+let longtext = "the concept of immutability is cruicial when you are working";
+let truncatedtext = longtext.substring(0, 20) + "...";
 console.log(truncatedtext);
-
-
