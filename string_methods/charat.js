442ef328fee7b4447d843791978d63067e05d2ec
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-27 21:39:08 +0530

    charAt in js

--- string_methods/charat.js
+++ string_methods/charat.js
@@ -1,13 +1,12 @@
-// charAt() in javascript 
+// charAt() in javascript
 
-const greeting='Hello world';
-let firstchar=greeting.charAt(0);
+const greeting = "Hello world";
+let firstchar = greeting.charAt(0);
 console.log(firstchar);
 
 //Ex:2
-const animals='elephant';
-const lastindex=animals.length-1;
+const animals = "elephant";
+const lastindex = animals.length - 1;
 
-let lastchar=animals.charAt(lastindex);
+let lastchar = animals.charAt(lastindex);
 console.log(lastchar);
-
