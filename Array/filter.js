ff3934ec72b1a0a20fe4c9ba44f7f48d578007bb
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-28 14:13:04 +0530

    filter in js

---
+++ Array/filter.js
@@ -0,0 +1,9 @@
+// filter in array method javavscript 
+/*
+In javavscript  filter method create a new array with all elements
+that pass a task specified using callback function 
+.ts consits three arugments(elements,index,array)
+syntax:
+array.filter()
+*/
+//ex:1 
