374edeb9318f03ef803dac39a345b3ddc0dd4c2a
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-28 12:33:19 +0530

    array in js

--- Array/array.js
+++ Array/array.js
@@ -4,3 +4,21 @@ let a=[10,20,30,40,50];
 console.log(a[2]);
 
 
+//Ex:2
+const fruits=['apple','orange','banana'];
+console.log(fruits);
+
+//Ex:3
+let emptyarray=[];
+let personnames=['venkat','yuva','Arish','arya'];
+emptyarray.push(personnames);
+console.log(emptyarray);
+
+//ex:4
+let dailyactivites=['eat','work','sleep'];
+console.log(dailyactivites[1]);
+
+//Ex:5
+let animals=['dog','bird'];
+animals.push('cat');
+console.log(animals);
