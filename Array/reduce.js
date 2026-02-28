affa90ea02704a69a902528f312e06b12b420d10
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-28 15:28:49 +0530

    reduce in js

---
+++ Array/reduce.js
@@ -0,0 +1,8 @@
+// Reduce in array methods javacsript 
+/*
+In javascript reduce() method reduce an array to a single value using callback function.
+the callback function accumulates a result based on provided logic.
+
+syntax:
+array.reduce(callback,initialvalue)
+*/
