ce77f35e9614abc216fe8c8f9698c92f5f19de8f
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-01 19:28:10 +0530

    fill in js

--- Array/fill.js
+++ Array/fill.js
@@ -1,1 +1,22 @@
 // fill in javavscript array methods
+
+/** */
+
+
+//Ex:1
+let n=[1,2,3,4,5];
+console.log(n.fill(20));
+
+//Ex:2
+let numbers=[10,20,30,40,50];
+console.log(numbers.fill(3,20));
+
+//Ex:3
+let array=[1,2,3,4,5];
+array.fill(0);
+console.log(array);
+
+//Ex:4
+array=[1,2,3,4,5];
+array.fill(0,2,4);
+console.log(array);
