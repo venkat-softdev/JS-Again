84890ef48e5ceb528877961fcfcde2492355ec86
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-01 10:34:26 +0530

    concat in js

---
+++ Array/concat.js
@@ -0,0 +1,14 @@
+// concat in array methods javascript
+/*
+Concat combines two or more array into a new array without changing the original array
+
+this method original array remains unchanged as the method is non-mutating
+syntax:
+array1.concat(array2)
+*/
+//Ex:1
+
+const a=[10,20,30];
+const b=[40,50];
+const c=a.concat(b);
+console.log(c);
