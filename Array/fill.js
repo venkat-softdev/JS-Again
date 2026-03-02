3c2804cfd3767cce4b2739d65ca877514fc6d397
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-02 15:56:25 +0530

    fill in js

--- Array/fill.js
+++ Array/fill.js
@@ -1,6 +1,12 @@
 // fill in javavscript array methods
 
-/** */
+/*
+In javascript fill method replacing an array elements with specific values
+ex filling [1,2,3]with "A" gives['A','A','A']
+
+syntax:
+array.fill();
+*/
 
 
 //Ex:1
@@ -20,3 +26,6 @@ console.log(array);
 array=[1,2,3,4,5];
 array.fill(0,2,4);
 console.log(array);
+
+//Ex:5
+
