5766f6801dca984c42ba6a7585457045004ca418
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-03 23:50:17 +0530

    join in js

---
+++ Array/join.js
@@ -0,0 +1,12 @@
+// join method in array javascript 
+/**
+ in Javscript join method join all elements of an array into a string using a specified separator 
+ if no seprator is provided it's default comma;
+ syntax:
+ array.join(seprator)
+ */
+
+ //Ex:1
+ const products=['pen','pencil','scale','eraser'];
+ console.log(products.join('|'));
+ 
