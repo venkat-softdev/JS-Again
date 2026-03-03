3d2ee509b0e0f51f860833fc177740d443862302
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-03 23:31:00 +0530

    includes in js

--- Array/includes.js
+++ Array/includes.js
@@ -3,3 +3,8 @@
  includes checks the specified element exists in array 
  if element is found it returns true otherwise return false.
 */
+
+//ex:1
+const products=['pen','pencil','rubber','scale'];
+console.log(products.includes('pen'));
+console.log(products.includes('box'));
