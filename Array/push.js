d1744a369ab50c01585dadcd9e6d8071621cbdcf
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-04 00:13:25 +0530

    push in js

---
+++ Array/push.js
@@ -0,0 +1,14 @@
+// push in array method javacsript 
+/**
+ in javscript push() method add one or more elements in end of the array
+ its modify original array and returns newlength of array 
+ syntax:
+ array.push()  
+ */
+
+ //ex:1
+ let n=[1,2,3,4,5];
+ console.log(n.push(6));
+ console.log(n);
+ 
+ 
