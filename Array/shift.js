d0eac67f8791b1de6d30cb1f390e85cd30689339
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-04 23:06:26 +0530

    shift in js

---
+++ Array/shift.js
@@ -0,0 +1,9 @@
+// shift in array method javascript 
+/**
+ in javascript shift() method remove the first element of an array 
+ it's modify original array and returns the removed elements
+ in this array no elemnet is found it returns the undefined 
+ */
+
+ //ex:1
+ 
