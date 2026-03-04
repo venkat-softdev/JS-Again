ba90093907d5ef0c4e686e8705f04bfd53e1dc45
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-04 22:52:19 +0530

    pop in js

---
+++ Array/pop.js
@@ -0,0 +1,24 @@
+//pop method in array javacsript
+/**
+ pop() remove the last element of an array
+
+ it modifies original array and create a new length of an array
+
+ */
+//ex:1
+const users=['venkat','arish ','ram','rajesh'];
+console.log(users);
+console.log(users.pop());
+console.log(users);
+
+//ex:2
+const myfish=['angel','clown','mandrian','sturgeon'];
+const popped=myfish.pop();
+console.log(popped);
+console.log(myfish);
+
+//ex:3
+const plants=['brocolli','cauliflower','cabbege','kale','tomato'];
+console.log(plants.pop());
+console.log(plants);
+
