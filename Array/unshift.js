017c390b1096e368f80ef027e4ede22379b8b5a3
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-04 23:21:00 +0530

    unshift in js

--- Array/unshift.js
+++ Array/unshift.js
@@ -3,3 +3,17 @@
  in javascript unshift() method add or more elements in begining of the array
  it modify the original array and returns new length of array
  */
+
+ //ex:1
+
+ const arr=[1,2];
+ arr.unshift(0);
+ console.log(arr);
+ 
+ //ex:2
+ const array=[1,2,3];
+ array.unshift(4,5);
+ console.log(array);
+ 
+ //ex:3
+ 
