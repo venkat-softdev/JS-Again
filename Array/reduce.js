2133ab0d0b68ed07d5beb00c6ab354295eee77bb
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-05 23:03:59 +0530

    reduce in js

--- Array/reduce.js
+++ Array/reduce.js
@@ -1,4 +1,4 @@
-// Reduce in array methods javacsript 
+// Reduce in array methods javacsript
 /*
 In javascript reduce() method reduce an array to a single value using callback function.
 the callback function accumulates a result based on provided logic.
@@ -9,44 +9,47 @@ array.reduce(callback,initialvalue)
 
 //Ex:1
 
-let numbers=[1,2,3,4,5];
-let sum=numbers.reduce((accumulator,value)=>{
-    return accumulator+value
+let numbers = [1, 2, 3, 4, 5];
+let sum = numbers.reduce((accumulator, value) => {
+  return accumulator + value;
 });
 console.log(sum);
 
 //Ex:2
-let nestedarray=[[1,2],[3,4],[5,6]];
-let flattenedArry=nestedarray.reduce((accumulator,values)=>{
-    return accumulator.concat(values)
-})
+let nestedarray = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+let flattenedArry = nestedarray.reduce((accumulator, values) => {
+  return accumulator.concat(values);
+});
 console.log(flattenedArry);
 
 //Ex:3
-let colors=['red','blue','green','red','yellow'];
+let colors = ["red", "blue", "green", "red", "yellow"];
 
-let colorCount=colors.reduce((accumulator,currentvalue)=>{
-    if (currentvalue in accumulator) {
-        accumulator[currentvalue]++;
-    }
-    else{
-        accumulator[currentvalue]=1
-    }
-    return accumulator
-},{})
+let colorCount = colors.reduce((accumulator, currentvalue) => {
+  if (currentvalue in accumulator) {
+    accumulator[currentvalue]++;
+  } else {
+    accumulator[currentvalue] = 1;
+  }
+  return accumulator;
+}, {});
 console.log(colorCount);
 
 //Ex:4
-const array=[1,2,3,4,5];
-const initialvalue=0;
-const sumwithvalue=array.reduce((acc,value)=>{
-    return acc+value
-})
+const array = [1, 2, 3, 4, 5];
+const initialvalue = 0;
+const sumwithvalue = array.reduce((acc, value) => {
+  return acc + value;
+});
 console.log(sumwithvalue);
 
 //Ex:5
-const values=[[10,20],[30,40],[50]];
-let result=values.reduce((acc,value)=>{
-    return acc.concat(value)
-})
+const values = [[10, 20], [30, 40], [50]];
+let result = values.reduce((acc, value) => {
+  return acc.concat(value);
+});
 console.log(result);
