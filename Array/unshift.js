f9164a98b8c2b9bb8ff402d461eaafd3e4d550a5
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-05 23:05:04 +0530

    unshift in js

--- Array/unshift.js
+++ Array/unshift.js
@@ -4,20 +4,19 @@
  it modify the original array and returns new length of array
  */
 
- //ex:1
+//ex:1
 
- const arr=[1,2];
- arr.unshift(0);
- console.log(arr);
- 
- //ex:2
- const array=[1,2,3];
- array.unshift(4,5);
- console.log(array);
- 
- //ex:3
+const arr = [1, 2];
+arr.unshift(0);
+console.log(arr);
 
- let arr1=[4,5,6];
- arr1.unshift(1,2,3);
- console.log(arr1);
- 
+//ex:2
+const array = [1, 2, 3];
+array.unshift(4, 5);
+console.log(array);
+
+//ex:3
+
+let arr1 = [4, 5, 6];
+arr1.unshift(1, 2, 3);
+console.log(arr1);
