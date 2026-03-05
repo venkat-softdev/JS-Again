b00d56699d547057383efcb01b583ea522f60836
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-05 23:03:44 +0530

    pop in js

--- Array/pop.js
+++ Array/pop.js
@@ -6,24 +6,23 @@
 
  */
 //ex:1
-const users=['venkat','arish ','ram','rajesh'];
+const users = ["venkat", "arish ", "ram", "rajesh"];
 console.log(users);
 console.log(users.pop());
 console.log(users);
 
 //ex:2
-const myfish=['angel','clown','mandrian','sturgeon'];
-const popped=myfish.pop();
+const myfish = ["angel", "clown", "mandrian", "sturgeon"];
+const popped = myfish.pop();
 console.log(popped);
 console.log(myfish);
 
 //ex:3
-const plants=['brocolli','cauliflower','cabbege','kale','tomato'];
+const plants = ["brocolli", "cauliflower", "cabbege", "kale", "tomato"];
 console.log(plants.pop());
 console.log(plants);
 
 //ex:4
-const fruits=['banana','orange','Apple','mango'];
+const fruits = ["banana", "orange", "Apple", "mango"];
 console.log(fruits.pop());
 console.log(fruits);
-
