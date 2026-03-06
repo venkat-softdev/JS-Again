88400110beabb0929f2ba0640a33782cee655e41
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-06 23:29:34 +0530

    function in js

---
+++ functions/function.js
@@ -0,0 +1,7 @@
+// function  in javacsript
+/**
+ In javascript function is a reusable block of code it used to perform
+ a specific task.
+
+ its allowing for maintainablity and modularation and realability.
+ */
