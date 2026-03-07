c8519d9be2457099b71d1eda5f81da3bb28c3d2d
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-07 14:49:12 +0530

    function expression in js

---
+++ functions/function_exp.js
@@ -0,0 +1,20 @@
+// function expression in javascript 
+/**
+ In javascript function expresssion defines the function as a variable asignment 
+
+ */
+
+ //ex1
+
+ let addvalue=function (a,b) {
+    return a+b
+ }
+ console.log(addvalue(10,20));
+ 
+ //ex:2
+ let evenumbers=function (number) {
+    
+    return number%2==0
+ }
+ console.log(evenumbers(1));
+ 
