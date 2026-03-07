1c221f0c412e4af3a659c9d4e5d5963428554e84
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-07 23:31:22 +0530

    arrow function in js

--- functions/arrowfun.js
+++ functions/arrowfun.js
@@ -12,3 +12,23 @@ let doublevalue=numbers.map((num)=>{
     return num*2
 })
 console.log(doublevalue);
+
+//ex:4
+let values=[10,20,30,40,50];
+let max=values[0];
+values.forEach((value)=>{
+    if (value>max) {
+        max=value
+    }
+})
+console.log(max);
+
+//ex:5
+let value=[1,20,30,40,50];
+let min=value[0]
+value.forEach((val)=>{
+    if (val<min) {
+        min=val
+    }
+})
+console.log(min);
