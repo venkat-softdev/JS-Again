403d86352d9b3583892dfd10f6773e8abef20001
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-08 12:39:38 +0530

    scope in js

---
+++ functions/scope.js
@@ -0,0 +1,15 @@
+// block scope and function scope in javavscript 
+
+//ex:1
+if (true) {
+    console.log('Hi venkat');
+    
+}
+
+//ex:2
+if (true) {
+    let blockscope='im a block scope variable'
+    console.log(blockscope);
+    
+}
+console.log(blockscope);
