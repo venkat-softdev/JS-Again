0fa1e14d09f4fda00d31628f1dfc2cd9c39f49a9
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-09 23:52:21 +0530

    ex:1 in js

--- easy_problems/ex1.js
+++ easy_problems/ex1.js
@@ -1,3 +1,25 @@
 // in javascript function is a reusable block of code it used to perform a specific task
 // it allows modularity,maintainability and modularation.
 
+//print numbers 1to 10 
+
+for (let i = 0; i < 10; i++) {
+    
+    console.log(i);
+    
+    
+}
+console.log('next value');
+
+// ex:2
+for (let i = 0; i <=10; i++) {
+    console.log('The value is :',i);
+    
+    
+}
+
+for (let i = 10; i>=0  ; i--) {
+    console.log(i);
+    
+    
+}
