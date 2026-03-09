c44e7446485b31936b099eed72b934901c10bce1
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-09 23:23:00 +0530

    function.js

--- functions/function.js
+++ functions/function.js
@@ -6,96 +6,81 @@
  its allowing for maintainablity and modularation and realability.
  */
 
+//ex:1
+function add(a, b) {
+  return a + b;
+}
+console.log(add(10, 20));
 
- //ex:1
- function add(a,b) {
-    return a+b
- }
- console.log(add(10,20));
- 
- //EX:2
- function square(number){
-   return number*number
- }
- console.log(square(5));
- 
- //ex:3
- const multiply=function (x,y) {
-    return x*y
- }
- console.log(multiply(5,2));
- 
- //ex:4
- const sum=(a,b)=>{
-    console.log(a+b);
-    return a+b
-    
- }
- console.log(sum(5,3));
- 
- //ex:5
- function addvalue() {
-    let total=0;
-    for (let i = 0; i < arguments.length; i++) {
-        total+=arguments[i]
-        
-    }
-    return total
- }
+//EX:2
+function square(number) {
+  return number * number;
+}
+console.log(square(5));
 
-console.log(addvalue(10,40,50));
+//ex:3
+const multiply = function (x, y) {
+  return x * y;
+};
+console.log(multiply(5, 2));
+
+//ex:4
+const sum = (a, b) => {
+  console.log(a + b);
+  return a + b;
+};
+console.log(sum(5, 3));
+
+//ex:5
+function addvalue() {
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
+}
+
+console.log(addvalue(10, 40, 50));
 
 function total(...args) {
-    let total=0
-    for (let i = 0; i < args.length; i++) {
-        total+=args[i]
-        
-    }
-    return total
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
 }
-console.log(total(10,20,30,40,50,60,70));
+console.log(total(10, 20, 30, 40, 50, 60, 70));
 
 //ex:7
 function greet() {
-    console.log('Hello Venkat');
-    
+  console.log("Hello Venkat");
 }
-greet()
-
+greet();
 
 //ex:8
 function printmsg() {
-    console.log('Welcome to javacsript functions');
-    
+  console.log("Welcome to javacsript functions");
 }
-printmsg()
+printmsg();
 
 //ex:8
-function addsum(a,b) {
-    
-    return a+b
+function addsum(a, b) {
+  return a + b;
 }
-console.log(addsum(100,200));
+console.log(addsum(100, 200));
 
 //ex:9
 function evenum(num) {
-    if (num%2==0) {
-        console.log('even number');
-        
-    } else {
-        console.log('odd number');
-        
-    }
-    
+  if (num % 2 == 0) {
+    console.log("even number");
+  } else {
+    console.log("odd number");
+  }
 }
-evenum(20)
+evenum(20);
 
 //ex:9
-function addnumbers(a,b) {
-    
-    return a+b
-
+function addnumbers(a, b) {
+  return a + b;
 }
-console.log(addnumbers(30,70));
-
- 
+console.log(addnumbers(30, 70));
