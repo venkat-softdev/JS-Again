8779c90a3296fdee3989f9ce9aa7e4d1e4e7af94
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-10 00:02:11 +0530

    ex:2 in js

--- easy_problems/ex2.js
+++ easy_problems/ex2.js
@@ -7,3 +7,17 @@ for (let i = 0; i < 10; i++) {
     }
     
 }
+
+//ex:2
+for (let i = 0; i <20; i++) {
+    
+    if (i%2==0) {
+        console.log(`hey its evennumber = ${i}` );
+        
+    }
+    else if (i%3==0) {
+        console.log(`hey it's oddnumber = ${i}`);
+        
+    }
+    
+}
