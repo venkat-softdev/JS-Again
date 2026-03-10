ef56ede8b4280d883126256bca3af68f2a0deb02
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-10 23:19:23 +0530

    ex:5 in js

---
+++ easy_problems/ex5.js
@@ -0,0 +1,25 @@
+// ex:5 in javascript 
+
+//ex:1
+
+function largest(a,b,c) {
+    
+    if (a>b && a>c) {
+        console.log(a);
+        
+
+    }
+    else if (b>a && b>c) {
+        console.log(b);
+        
+    }
+    else if (c>a && c>b) {
+        console.log(c);
+        
+    }
+   
+    return 
+     
+}
+
+console.log(largest(1000,2000,3000));
