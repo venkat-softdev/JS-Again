a46819acc4ab89ee49dce33aabdd169fc44def61
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-10 23:03:32 +0530

    ex4 in js

--- easy_problems/ex4.js
+++ easy_problems/ex4.js
@@ -6,3 +6,13 @@ function largest(a,b) {
     return a>b?a:b;
 }
 console.log(largest(90,20));
+
+//ex:2
+let arrays=[10,20,30,409,50];
+let max=arrays[0];
+arrays.forEach((arr)=>{
+    if (arr > max) {
+        max=arr
+    }
+})
+console.log(max);
