5f49a477765c2770183bdc8996d4c86da3f624c1
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-10 23:34:29 +0530

    ex6 in js

---
+++ easy_problems/ex6.js
@@ -0,0 +1,14 @@
+// ex:6 iun javascript 
+
+// find odd or even 
+
+let numbers=[1,2,3,4,5];
+let evennums=[];
+numbers.forEach((num)=>{
+    if (num%2==0) {
+        evennums.push(num)
+    }
+})
+console.log(evennums);
+
+//ex:2
