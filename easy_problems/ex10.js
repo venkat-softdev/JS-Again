14288af38360dc5278c8d4b660a6bd6de1b9d280
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-11 14:35:59 +0530

    ex:10 in js

---
+++ easy_problems/ex10.js
@@ -0,0 +1,32 @@
+//ex:10 in javascript 
+
+// count vowels in javascript
+
+function countVowels(str){
+  let count = 0;
+  let vowels = "aeiou";
+
+  for(let char of str.toLowerCase()){
+    if(vowels.includes(char)){
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countVowels("aeiow"));
+
+//ex:2
+function count(value) {
+    
+    let count=0;
+    let vowels='aeiou'
+    for (const char2 of value) {
+        if (vowels.includes(char2)) {
+            count++
+        }
+    }
+    return count
+}
+console.log(count('venkat'));
