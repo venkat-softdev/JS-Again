c96829037272e3c3387a62ed3926904c426ada56
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-11 14:52:19 +0530

    ex12 in js

---
+++ easy_problems/ex12.js
@@ -0,0 +1,15 @@
+// swap two numbers in javascript 
+
+//ex:1
+let a=10;
+let b=20;
+
+[a,b]=[b,a];
+console.log(a,b);
+
+//ex:2
+let fname='venkat';
+let lname='v';
+
+[fname,lname]=[lname,fname];
+console.log(fname,lname);
