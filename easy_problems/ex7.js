772a617a0d11c50ef604824c70cb3a47b341a01e
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-11 11:56:48 +0530

    ex7 in js

--- easy_problems/ex7.js
+++ easy_problems/ex7.js
@@ -25,3 +25,14 @@ function fun(j) {
     return result
 }
 console.log(fun(7));
+
+//ex:3
+function fact(k) {
+    let result=1
+    for (let i = 1; i < k; i++) {
+        result=result*i
+        
+    }
+    return result
+}
+console.log(fact(5));
