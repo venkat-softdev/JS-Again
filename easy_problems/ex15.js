59f71ecd4e075e7562e259c9cbe9bd311f7cad4e
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-11 15:15:02 +0530

    ex:15 in js

---
+++ easy_problems/ex15.js
@@ -0,0 +1,18 @@
+// ex:15 in javascript 
+
+// simple interest 
+
+function simpleinterrest(p,r,t) {
+    
+    return (p * r * t) /100 
+}
+console.log(simpleinterrest(1000,5,2));
+
+//ex:2
+
+let str1=2000;
+let str2=6;
+let str3=5;
+
+let result=str1*str2*str3/100;
+console.log(result);
