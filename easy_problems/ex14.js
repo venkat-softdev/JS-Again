d1a2875bb12006fbf916a26ee43154ab8598c942
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-11 15:08:28 +0530

    ex:14 in js

---
+++ easy_problems/ex14.js
@@ -0,0 +1,15 @@
+//ex:14 in javascript 
+
+// Area of circle
+
+function area(radius) {
+    
+    return Math.PI * radius *radius;
+}
+console.log(area(5));
+
+let value=3;
+
+let result=Math.PI*value*value;
+
+console.log(result);
