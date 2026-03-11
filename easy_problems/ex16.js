6052379048bd4e2702fc63cd48d713da977bbf66
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-11 15:24:08 +0530

    ex:16 in js

---
+++ easy_problems/ex16.js
@@ -0,0 +1,33 @@
+//ex:16 in javascript 
+
+// multiplication table;
+
+let num=5;
+for (let i = 1; i <=10; i++) {
+    console.log(num +'X'+i+'='+num*i);
+    
+    
+}
+
+console.log('Next table');
+
+//x:2
+
+let number=2;
+for (let i = 1; i <= 10; i++) {
+    
+    console.log(number +'x'+i+'='+number*i);
+    
+    
+}
+
+console.log('another next atble');
+
+//ex:3
+
+let num2=3;
+for (let i = 1; i<=10; i++) {
+    console.log(num2 +'x'+i+'='+num2*i);
+    
+    
+}
