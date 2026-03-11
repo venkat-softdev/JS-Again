693c061b632e0dbc4032065cb9580f6dd6718ef8
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-11 15:04:34 +0530

    ex:13 in js

---
+++ easy_problems/ex13.js
@@ -0,0 +1,16 @@
+//example 13 in javascript
+
+// find square in number
+
+function square(num) {
+    
+    return num*num
+}
+console.log(square(5));
+
+//ex:2
+
+let value=4;
+let squarevalue=value*value;
+console.log(squarevalue);
+
