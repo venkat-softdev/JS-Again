f06f9a6d34dc3cde2152d290400366c6a297dd05
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-14 23:27:42 +0530

    rest and spread operator in js

--- functions/rest_spread.js
+++ functions/rest_spread.js
@@ -1,36 +1,30 @@
-// rest and spread operator in javacsript 
+// rest and spread operator in javacsript
 
-function myfunction(first,second,...venkat) {
-    console.log(first);
-    console.log(second);
-    console.log(venkat);
-    
-    
-    
+function myfunction(first, second, ...venkat) {
+  console.log(first);
+  console.log(second);
+  console.log(venkat);
 }
-myfunction(10,20,30,40, 50 ,60);
+myfunction(10, 20, 30, 40, 50, 60);
 
 //spread operator
-let mynum=[1,2,3,4];
-let newarry=[...mynum,5,6];
+let mynum = [1, 2, 3, 4];
+let newarry = [...mynum, 5, 6];
 console.log(mynum);
 console.log(newarry);
 
 // ex:2
-let mynames=['venkat','aqua'];
-let nwename=[...mynames,'yuva','jignesh'];
+let mynames = ["venkat", "aqua"];
+let nwename = [...mynames, "yuva", "jignesh"];
 console.log(mynames);
 console.log(nwename);
 
 //ex:3
-function myvalues(firstvalue,secondvalue,...thirdvalue) {
-    console.log(firstvalue);
-    console.log(secondvalue);
-   console.log(thirdvalue);
-   
-    
-    return firstvalue+secondvalue;
-    
-}
-console.log(myvalues(10,20,30,40,60));
+function myvalues(firstvalue, secondvalue, ...thirdvalue) {
+  console.log(firstvalue);
+  console.log(secondvalue);
+  console.log(thirdvalue);
 
+  return firstvalue + secondvalue;
+}
+console.log(myvalues(10, 20, 30, 40, 60));
