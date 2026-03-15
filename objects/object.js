95328a8796b9a11e40af0bc9d2761c9d1e80eb9e
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-15 12:26:26 +0530

    objects in js

---
+++ objects/object.js
@@ -0,0 +1,18 @@
+// object in javascript 
+/*
+ in javascript object is a compls data type it represent collection of 
+ properties and their values.
+
+ syntax:
+ object={}
+*/
+
+let person={
+    name:'venkat',
+    age:20,
+    city:'vadalur',
+}
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(Object.entries(person));
+
