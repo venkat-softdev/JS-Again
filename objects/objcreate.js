6c5a3b25c1674fcf67736ca3eefa8ebfd7187279
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-15 12:52:52 +0530

    object craete in js

--- objects/objcreate.js
+++ objects/objcreate.js
@@ -1,8 +1,20 @@
 // object create method in javascript 
-const proto={}
+const proto={
+    eligible:function () {
+        if (this.age>18) {
+            console.log(`${this.name} eligible to vote`);
+            
+        } else {
+            console.log(`${this.name} not eligible for vote`);
+            
+        }
+    }
+}
 const people=Object.create(proto);
 people.name='venkat';
 people.age=20;
 people.city='vadalur'
 console.log(people);
+people.eligible()
+
 
