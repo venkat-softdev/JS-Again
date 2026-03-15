96d4247b52275656f5ad26be80da2912b307b3a9
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-15 12:37:06 +0530

    objects in js

--- objects/object.js
+++ objects/object.js
@@ -16,3 +16,11 @@ console.log(Object.keys(person));
 console.log(Object.values(person));
 console.log(Object.entries(person));
 
+// object constructor 
+
+const car=new Object();
+car.make='maruthi';
+car.model='M800';
+car.year=2007;
+console.log(car);
+console.log(car.model);
