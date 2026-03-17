33bc506c6fd004187dcecebda2172310ce35a7c6
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-17 15:14:39 +0530

    iterate the object in js

---
+++ objects/iterateobj.js
@@ -0,0 +1,16 @@
+// iterate the objects 
+
+//ex:1
+const fruits={
+    name:'apple',
+    color:'red'
+};
+
+for (const key in fruits) {
+    console.log(`${key} : ${fruits[keys]}`);
+    
+    
+    
+    
+    
+}
