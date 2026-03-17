827ba493ee67627ea86b099d13bfaaa013422baf
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-17 15:46:46 +0530

    obj keys method in js

--- objects/keys_obj.js
+++ objects/keys_obj.js
@@ -18,3 +18,34 @@ Object.entries(fruits).forEach((key)=>{
     console.log(key);
     
 })
+
+//ex:1
+let user={
+    name:'venkat',
+    age:20,
+    city:'vadalur'
+}
+console.log(Object.keys(user));
+
+//ex:2
+// store a variable
+
+let users={
+ name:'venkat',
+ age:20
+}
+let keys=Object.keys(users);
+console.log(keys);
+
+//ex:3 using loop 
+
+let member={
+    name:'venkat',
+    age:20
+};
+
+let key=Object.keys(member);
+key.forEach((keys)=>{
+    console.log(keys);
+    
+})
