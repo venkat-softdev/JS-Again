fbaccc724eae6665e7bb093e3a681d6084dc0744
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-17 16:09:04 +0530

    obj keys method in js

--- objects/keys_obj.js
+++ objects/keys_obj.js
@@ -1,5 +1,10 @@
 // object keys method in javascript 
 
+/**
+  in javascript objects keys method return of an array all keys
+  [property name] in object.
+ */
+
 let fruits={
     name:'banana',
     color:'yellow',
@@ -63,4 +68,24 @@ let fuser={
 Object.keys(fuser).forEach((key)=>{
     console.log(key,fuser[key]);
     
-})
+});
+//ex:5 count properties
+
+let suser={
+    name:'yuva',
+    age:15
+}
+console.log(Object.keys(suser).length);
+
+//ex:6 (empty object)
+
+let obj={};
+console.log(Object.keys(obj));
+
+//ex:7  interview case 
+
+let mem={
+    name:'venkat'
+}
+let store=Object.create(mem);
+console.log(Object.keys(store));
