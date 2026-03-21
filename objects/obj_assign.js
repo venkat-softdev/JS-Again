4bcacdf7ce61e26d561308389d365afcd7bb9101
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-21 13:13:53 +0530

    obj assign method in js

--- objects/obj_assign.js
+++ objects/obj_assign.js
@@ -1,3 +1,14 @@
 // object assign method in javascript 
 // in javascript object.assign() method is used to copy or merge properties from one
 //  or more objects into a target objects 
+
+
+//ex:1
+const obj1={
+    a:1
+}
+const obj2={
+    b:2
+}
+const merged=Object.assign(obj1,obj2)
+console.log(merged);
