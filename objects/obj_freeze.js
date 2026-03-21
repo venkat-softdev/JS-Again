4a30e21f0e475b606df00febfe8439e93da98b5f
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-21 13:49:15 +0530

    obj freeze method in js

--- objects/obj_freeze.js
+++ objects/obj_freeze.js
@@ -16,3 +16,12 @@
   freezeobj.gender="male"
   console.log(freezeobj);
   
+  //ex:2 normal objects
+  let normalobj={
+    name:'venkat',
+    age:20
+  }
+  normalobj.gender="male"
+ normalobj.name="yuva"
+  console.log(normalobj);
+  
