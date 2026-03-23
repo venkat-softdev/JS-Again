6805fab402296b353387ffeb6cabe85d91f7177f
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-23 22:49:38 +0530

    obj fromentries method in js

--- objects/obj_froment.js
+++ objects/obj_froment.js
@@ -1,15 +1,13 @@
-// object fromEntries method in javascript 
+// object fromEntries method in javascript
 /**
  in javascript object.fromEntries() method it used to converts list of key  value
  pairs into a objects
  */
 
- //ex:1
- let data=[
-    ['name','venkat'],
-    ['age',21]
- ]
- let obj=Object.fromEntries(data)
- console.log(obj);
- 
- 
+//ex:1
+let data = [
+  ["name", "venkat"],
+  ["age", 21],
+];
+let obj = Object.fromEntries(data);
+console.log(obj);
