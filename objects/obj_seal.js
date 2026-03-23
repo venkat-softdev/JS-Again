85428eef650e252458c311e3a6cdf68bad18322f
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-23 22:50:46 +0530

    obj seal method in js

--- objects/obj_seal.js
+++ objects/obj_seal.js
@@ -1,4 +1,4 @@
-// object seal() method in javascript 
+// object seal() method in javascript
 /**
   in javscript object.seal() is a method that prevents 
   adding and deletinga properties
@@ -6,32 +6,27 @@
   but they allow only modifying the existing objects .
  */
 
-  //ex:1
-  const sealobj=Object.seal(
-    {name:'venkat',
-        age:20
-    }
-  )
-  sealobj.name="yuva"
-  sealobj.gender="male"
-  console.log(sealobj);
-  
-  //ex:2
-  let user={
-    name:'venkat',
-    age:20
-  }
-  user.name="yuva"
-  Object.seal(user)
-  user.city='chennai'
-  console.log(user);
-  
-  //ex:3 delete not allowed 
+//ex:1
+const sealobj = Object.seal({ name: "venkat", age: 20 });
+sealobj.name = "yuva";
+sealobj.gender = "male";
+console.log(sealobj);
 
-  let users={
-    name:'yuva'
-  }
-  Object.seal(users)
-  delete user.name;
-  console.log(users);
-  
+//ex:2
+let user = {
+  name: "venkat",
+  age: 20,
+};
+user.name = "yuva";
+Object.seal(user);
+user.city = "chennai";
+console.log(user);
+
+//ex:3 delete not allowed
+
+let users = {
+  name: "yuva",
+};
+Object.seal(users);
+delete user.name;
+console.log(users);
