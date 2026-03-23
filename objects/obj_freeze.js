23380f407317b8c6446417a67920ad844614cde8
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-23 22:50:06 +0530

    obj freeze method in js

--- objects/obj_freeze.js
+++ objects/obj_freeze.js
@@ -1,27 +1,22 @@
-// object.freeze method in javascript 
+// object.freeze method in javascript
 /**
   in javascript object.freeze() method that completely prevents the
   change over the objects.
   2. you cannot add,delete and modify properties.
  */
 
-  //ex:1
-  const freezeobj=Object.freeze(
-    {name:'venkat',
-        age:20
-    }
-  )
+//ex:1
+const freezeobj = Object.freeze({ name: "venkat", age: 20 });
 
-  freezeobj.name="yuva"
-  freezeobj.gender="male"
-  console.log(freezeobj);
-  
-  //ex:2 normal objects
-  let normalobj={
-    name:'venkat',
-    age:20
-  }
-  normalobj.gender="male"
- normalobj.name="yuva"
-  console.log(normalobj);
-  
+freezeobj.name = "yuva";
+freezeobj.gender = "male";
+console.log(freezeobj);
+
+//ex:2 normal objects
+let normalobj = {
+  name: "venkat",
+  age: 20,
+};
+normalobj.gender = "male";
+normalobj.name = "yuva";
+console.log(normalobj);
