6e6e673ec44bc556eaa9c473cb89d9e187422b75
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-24 23:48:05 +0530

    optional chaining in js

--- objects/opt_chain.js
+++ objects/opt_chain.js
@@ -23,3 +23,42 @@ console.log(user);
 
 //using optional chaining 
 console.log(user.address?.city);
+
+//ex:2
+let keys='city'
+console.log(user.address?.[keys]);
+
+//ex:3
+
+const user_details={
+    userId:'456',
+    account:{
+        accountNumber:'ABCDE123',
+        holder:{
+            profile:{
+            firstname:'venkat',
+            lastname:'vp',
+            birthdate:'2006-05-01'
+            }
+           
+        },
+        contact:{
+            address:{
+                city:'vadalur',
+                state:'Tamil Nadu',
+                country:'India'
+            },
+            phone:[
+                {
+                    type:'Home',
+                    phoneNumber:'12346889'
+                },
+                {
+                    type:'office',
+                    phoneNumber:'987654'
+                }
+            ]
+        }
+    }
+}
+console.log(user_details);
