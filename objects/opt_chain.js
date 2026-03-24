b11f3fb5d464ffbb3feff987698a5652d45600d3
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-24 23:38:03 +0530

    optional chaining in js

--- objects/opt_chain.js
+++ objects/opt_chain.js
@@ -1,8 +1,25 @@
+
+// optional chaiining in javascript
+
+
 let user={
     name:'venkat',
+
     address:{
         city:'vadalur'
     }
+    
 }
 console.log(user);
-console.log(user.address.city);
+
+
+//using ternaary operator
+
+// let result=user.address ? address:undefined
+// console.log(result);
+
+// //option-2
+// console.log(user.address && user.address.city);
+
+//using optional chaining 
+console.log(user.address?.city);
