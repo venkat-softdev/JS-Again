f883ee55d6b5e2a6256c9739c6823e40c0ecfd42
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-26 22:54:34 +0530

    dom in js

--- Dom/dom.js
+++ Dom/dom.js
@@ -1,3 +1,8 @@
 document.title="venkat javascript"
 console.dir(document);
 
+let h1=document.getElementById('h1')
+h1.style.color="gold"
+h1.style.backgroundColor="teal"
+h1.style.padding="10px"
+h1.style.borderRadius="5px"
