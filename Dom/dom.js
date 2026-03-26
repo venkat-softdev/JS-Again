9df007704010d877dbeef147ad2753948bf3455d
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-26 23:03:34 +0530

    dom in js

--- Dom/dom.js
+++ Dom/dom.js
@@ -6,3 +6,12 @@ h1.style.color="gold"
 h1.style.backgroundColor="teal"
 h1.style.padding="10px"
 h1.style.borderRadius="5px"
+
+let box=document.querySelector('.box')
+box.style.padding="10px"
+box.style.backgroundColor="grey"
+box.style.color="#fff"
+box.style.width="500px"
+box.style.height="300px"
+box.style.margin="10px"
+box.style.borderRadius="10px"
