99b4704c47b794917fda75bcd162b4302bcf884b
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-27 23:36:55 +0530

    dom2 in js

---
+++ Dom/dom2.js
@@ -0,0 +1,16 @@
+let para=document.getElementsByTagName('p')
+console.log(para);
+
+const parent=para[0].parentNode;
+console.log(parent);
+parent.style.backgroundColor="purple"
+parent.style.padding="10px"
+parent.style.color="#fff";
+
+
+let head=document.getElementsByTagName('h2');
+console.log(head);
+let parent2=head[0].parentNode;
+console.log(parent2);
+parent2.style.backgroundColor="lightgreen"
+parent2.style.padding="30px";
