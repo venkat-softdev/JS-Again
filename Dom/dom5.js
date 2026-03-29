3fee76eb9d460436891aa2fbc5fa73f2511d8158
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-29 11:58:48 +0530

    dom5 in js

---
+++ Dom/dom5.js
@@ -0,0 +1,19 @@
+let body=document.querySelector('body');
+body.style.fontFamily="poppins"
+
+let clockdiv=document.querySelector('.clock');
+
+function clock() {
+    let date=new Date()
+    let time=date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+    clockdiv.innerHTML=time;
+}
+
+setInterval(() => {
+    clock()
+}, 1000);
+
+clockdiv.style.color="blue"
+clockdiv.style.textAlign="center"
+clockdiv.style.fontSize="100px"
+clockdiv.style.fontWeight="900"
