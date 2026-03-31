c056e9e207df2c48ce28d06cc99203da6afe6593
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-31 16:36:26 +0530

    mouse event in js

--- events/event.js
+++ events/event.js
@@ -5,3 +5,23 @@ let btn=document.querySelector('#btn')
 btn.addEventListener('click',function () {
     alert('welcome the javascript events')
 })
+btn.style.backgroundColor='blue'
+btn.style.color="#fff"
+btn.style.padding="15px"
+btn.style.width="200px"
+btn.style.border="none";
+btn.style.borderRadius="4px"
+btn.style.fontSize="15px"
+btn.style.fontWeight="900"
+
+//mouse events in javascript;
+/**
+ double click
+ click
+ */
+
+ let btn2=document.querySelector('#dblbtn');
+ btn2.addEventListener('dblclick',function () {
+ 
+    alert('This is double click Button')
+ })
