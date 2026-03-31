fd489fb9fef945ee589c7e0072b1f834f898e352
venkatsoftdev <softdev.venkat@gmail.com>
2026-03-31 16:26:44 +0530

    events in j s

--- events/event.js
+++ events/event.js
@@ -1,2 +1,7 @@
 let body=document.querySelector('body');
-body.style.fontFamily="poppins"
+body.style.fontFamily="poppins";
+
+let btn=document.querySelector('#btn')
+btn.addEventListener('click',function () {
+    alert('welcome the javascript events')
+})
