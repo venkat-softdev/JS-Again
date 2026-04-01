c91e18b7a79865a72018e22e237b75c4eee63bae
venkatsoftdev <softdev.venkat@gmail.com>
2026-04-01 23:32:08 +0530

    keyboard event in js

--- events/event2.js
+++ events/event2.js
@@ -1,2 +1,18 @@
 let body=document.querySelector('body')
 body.style.fontFamily='poppins'
+
+document.addEventListener('keydown',keyevent)
+document.addEventListener('keypress',keyevent);
+document.addEventListener('keyup',keyevent);
+
+function keyevent(event) {
+    const eventtype=event.type;
+    const keycode=event.code;
+    const keyname=event.key;
+    console.log(`event type : ${eventtype}`);
+    console.log(`event code : ${keycode}`);
+    console.log(`event name : ${keyname}`);
+    
+    
+    
+}
