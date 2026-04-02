bd9caa5103a5d999a6ba93070f9a33a6c7cfac9c
venkatsoftdev <softdev.venkat@gmail.com>
2026-04-02 22:32:19 +0530

    event2 in js

--- events/event2.js
+++ events/event2.js
@@ -1,5 +1,5 @@
-let body=document.querySelector('body')
-body.style.fontFamily='poppins'
+let body = document.querySelector("body");
+body.style.fontFamily = "poppins";
 
 // document.addEventListener('keydown',keyevent)
 // document.addEventListener('keypress',keyevent);
@@ -12,25 +12,20 @@ body.style.fontFamily='poppins'
 //     console.log(`event type : ${eventtype}`);
 //     console.log(`event code : ${keycode}`);
 //     console.log(`event name : ${keyname}`);
-    
-    
-    
+
 // }
 
+let label = document.querySelector("label");
+let input = document.querySelector("input");
+let error = document.querySelector("#error");
 
-let label=document.querySelector('label');
-let input=document.querySelector('input');
-let error=document.querySelector('#error')
+input.addEventListener("keydown", function (event) {
+  const key = event.key;
+  console.log(key);
 
-input.addEventListener('keydown',function (event) {
-    const key=event.key;
-    console.log(key);
-    
-    if (isNaN(key)) {
-        error.textContent='please enter num only'
-        
-    }
-    else{
-        error.textContent=''
-    }
-})
+  if (isNaN(key)) {
+    error.textContent = "please enter num only";
+  } else {
+    error.textContent = "";
+  }
+});
