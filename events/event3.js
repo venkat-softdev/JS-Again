7c0aa0ab3f965926fa4b4b51f973ceabf09c85ee
venkatsoftdev <softdev.venkat@gmail.com>
2026-04-03 23:12:05 +0530

    form event in js

--- events/event3.js
+++ events/event3.js
@@ -1,2 +1,17 @@
 let body=document.querySelector('body')
 body.style.fontFamily='poppins'
+
+
+//form event in js 
+
+const form=document.querySelector('form')
+const username=document.querySelector('#username')
+const email=document.querySelector('#email')
+const courses=document.querySelector('#course');
+const radiobtn=document.querySelectorAll('input[name="gender"]')
+const checkbox=document.querySelector('#agree')
+form.addEventListener('submit',function (e) {
+    e.preventDefault();
+  
+    
+})
