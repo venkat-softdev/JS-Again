3b21b31fdbfc5a7566ea1c4eff25b5756555f431
venkatsoftdev <softdev.venkat@gmail.com>
2026-04-04 21:14:51 +0530

    promise in js

---
+++ events/promise.js
@@ -0,0 +1,20 @@
+// javascript promise 
+
+let promise=new Promise((resolve, reject) => {
+    let sum=1+1
+
+    if (sum==2) {
+        resolve('done')
+    }
+    else{
+        reject('undone')
+    }
+})
+
+promise.then((msg)=>{
+ console.log(msg);
+ 
+}).catch((error)=>{
+    console.log(error);
+    
+})
