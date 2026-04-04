4a3c149647d1bacc9b065149ce2b35f492783306
venkatsoftdev <softdev.venkat@gmail.com>
2026-04-04 21:38:48 +0530

    promise in js

--- events/promise.js
+++ events/promise.js
@@ -42,3 +42,58 @@ settimeoutpromise(2000).then((msg)=>{
     
 })
 
+
+//ex:2
+let mypromise=new Promise((resolve, reject) => {
+    let sucees='true'
+    
+    if (sucees=='true') {
+        resolve('well done')
+    }
+    else{
+        reject('try again')
+    }
+})
+
+mypromise.then((good)=>{
+    console.log(good);
+    
+})
+.catch((error)=>{
+    console.log(error);
+    
+})
+
+//ex:3
+let ispass='true'
+
+let exam=new Promise((resolve, reject) => {
+    if (ispass) {
+        resolve('pass')
+    }
+    else{
+        reject('fail')
+    }
+})
+
+exam.then((msg)=>{
+    console.log(msg);
+    
+})
+.catch((error)=>{
+    console.log(error);
+    
+})
+
+//ex:4
+
+let download=new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('download successfully')
+    }, 2000);
+})
+
+download.then((msg)=>{
+    console.log(msg);
+    
+})
