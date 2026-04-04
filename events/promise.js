741ee43950b3d9ead7946a13fb4fc12a0f6823e7
venkatsoftdev <softdev.venkat@gmail.com>
2026-04-04 21:59:35 +0530

    promise in js

--- events/promise.js
+++ events/promise.js
@@ -97,3 +97,119 @@ download.then((msg)=>{
     console.log(msg);
     
 })
+
+//ex:5
+let checknumber=new Promise((resolve, reject) => {
+    let num=10
+
+    if (num>5) {
+        resolve('num greater than 5')
+    } else {
+        reject('num less than 5')
+    }
+})
+checknumber.then((msg)=>{
+    console.log(msg);
+    
+})
+.catch((error)=>{
+    console.log(error);
+    
+})
+
+//ex:6
+let num=0
+
+let checking=new Promise((resolve, reject) => {
+    if (num%2==0) {
+        resolve('even number')
+    } else {
+        reject('odd number')
+    }
+})
+
+checking.then((msg)=>{
+    console.log(msg);
+    
+})
+.catch((error)=>{
+    console.log(error);
+    
+})
+
+//ex:7
+
+let login=new Promise((resolve, reject) => {
+    let password='12345'
+
+    if (password==='12345') {
+        resolve('login success')
+    } else {
+        reject('wrong password try again')
+    }
+})
+
+login.then((msg)=>{
+    console.log(msg);
+    
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
+
+//ex:8
+let orderfood=new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('food delivered')
+    }, 3000);
+})
+
+orderfood.then((msg)=>{
+    console.log(msg);
+    
+})
+//ex:9
+
+let work=new Promise((resolve, reject) => {
+    resolve(10)
+})
+
+work.then((num)=>{
+    console.log(num);
+    return num*2
+    
+})
+.then((num)=>{
+    console.log(num);
+    return num+5
+    
+})
+.then((num)=>{
+    console.log(num);
+    return num/2
+    
+})
+
+//ex:10
+
+let randomvalue=new Promise((resolve, reject) => {
+    let random=Math.random();
+
+    if (random > 0.5) {
+        resolve('success')
+    } else {
+        reject('failed')
+    }
+})
+
+randomvalue.then((msg)=>{
+    console.log(msg);
+    
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
+
+//ex:11
