d0221377ee3fa9a355ae5594cc9861edef6b4cf1
venkatsoftdev <softdev.venkat@gmail.com>
2026-04-05 15:46:22 +0530

    async and await in js

--- events/async_await.js
+++ events/async_await.js
@@ -1,143 +1,189 @@
-// javascript async and await
+// // javascript async and await
 
-async function welcome(params) {
-    return'hello'
-}
-console.log(welcome());
+// async function welcome(params) {
+//     return'hello'
+// }
+// console.log(welcome());
 
-welcome().then((msg)=>{
-    console.log(msg);
+// welcome().then((msg)=>{
+//     console.log(msg);
     
-})
-.catch((err)=>{
-    console.log(err);
+// })
+// .catch((err)=>{
+//     console.log(err);
     
-})
+// })
 
-//ex:2
+// //ex:2
 
-async function getdata() {
+// async function getdata() {
     
-    let blogpost=new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('blog post')
+//     let blogpost=new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('blog post')
             
-        }, 2000);
-    })
+//         }, 2000);
+//     })
 
-    let blogcomment=new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('comments for the blog')
-        }, 3000);
-    })
-    console.log('fetching post....');
-    let post=await blogpost;
-    console.log('post :',post);
-    console.log('fetching comments');
-    let comment=await blogcomment;
-    console.log('comment :',comment);
-    return [post,comment];
+//     let blogcomment=new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('comments for the blog')
+//         }, 3000);
+//     })
+//     console.log('fetching post....');
+//     let post=await blogpost;
+//     console.log('post :',post);
+//     console.log('fetching comments');
+//     let comment=await blogcomment;
+//     console.log('comment :',comment);
+//     return [post,comment];
     
     
     
     
-}
-console.log('welcome to blog post');
-let output=getdata()
-console.log(output);
+// }
+// console.log('welcome to blog post');
+// let output=getdata()
+// console.log(output);
 
 
-//ex:3
+// //ex:3
 
 
 
-function getnumber() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(10)
-        }, 1000);
-    })
+// function getnumber() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(10)
+//         }, 1000);
+//     })
 
-}
+// }
 
-async function run() {
-    let data=await getnumber()
-     let adding=data+5;
-     console.log(adding);
+// async function run() {
+//     let data=await getnumber()
+//      let adding=data+5;
+//      console.log(adding);
      
-     console.log(adding*2);
+//      console.log(adding*2);
      
-}
-run()
+// }
+// run()
 
 
-//ex:2
+// //ex:2
 
-function getA(params) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(5)
-        }, 1000);
-    })
-}
+// function getA(params) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(5)
+//         }, 1000);
+//     })
+// }
 
-function getB(params) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(10)
-        }, 1000);
-    })
-}
+// function getB(params) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(10)
+//         }, 1000);
+//     })
+// }
 
-async function combine(params) {
+// async function combine(params) {
     
-    let result1= await getA()
-    let result2=await getB()
+//     let result1= await getA()
+//     let result2=await getB()
 
-    let result3= result1+result2;
-    console.log(result3);
+//     let result3= result1+result2;
+//     console.log(result3);
     
-}
-combine()
+// }
+// combine()
 
 
-//ex:3
+// //ex:3
 
-function checknum(num) {
+// function checknum(num) {
     
-    return new Promise((resolve, reject) => {
-        if (num >0) {
-            resolve('positive')
-        } else {
-            reject('negative')
-        }
-    })
-}
+//     return new Promise((resolve, reject) => {
+//         if (num >0) {
+//             resolve('positive')
+//         } else {
+//             reject('negative')
+//         }
+//     })
+// }
 
-async function run(params) {
+// async function run(params) {
     
-    try {
-        let res= await checknum(5)
+//     try {
+//         let res= await checknum(5)
         
-        console.log(res);
+//         console.log(res);
         
-    } catch (err) {
+//     } catch (err) {
        
-        console.log(err);
+//         console.log(err);
         
-    }
+//     }
 
-    try {
+//     try {
         
-        let value=await checknum(-2)
-        console.log(value);
+//         let value=await checknum(-2)
+//         console.log(value);
          
 
-    } catch (err) {
-        console.log(err);
+//     } catch (err) {
+//         console.log(err);
         
-    }
+//     }
+
+// }
+
+//ex:5
+
+function order() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('food order placed')
+        }, 1000);
+    })
+}
+
+function cook(params) {
+    
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve ('food cooking done')
+        }, 1000);
+    })
+}
+
+function delivered(params) {
+    
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve ('food delivered')
+        }, 1000);
+    })
+}
+
+
+async function food(params) {
+    
+    let orderfood=await order()
+    console.log(orderfood);
+
+    let cookingfood=await cook()
+    console.log(cookingfood);
+
+    let deliveredfood=await delivered()
+    console.log(deliveredfood);
+    
+    
+    
 
 }
 
-//ex:5
+food()
+
