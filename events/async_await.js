b871abede16bbcabc0418f00d41b902ab74027ab
venkatsoftdev <softdev.venkat@gmail.com>
2026-04-05 14:47:15 +0530

    async and await in js

--- events/async_await.js
+++ events/async_await.js
@@ -13,3 +13,42 @@ welcome().then((msg)=>{
     console.log(err);
     
 })
+
+//ex:2
+
+async function getdata() {
+    
+    let blogpost=new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('blog post')
+            
+        }, 2000);
+    })
+
+    let blogcomment=new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('comments for the blog')
+        }, 3000);
+    })
+    console.log('fetching post....');
+    let post=await blogpost;
+    console.log('post :',post);
+    console.log('fetching comments');
+    let comment=await blogcomment;
+    console.log('comment :',comment);
+    return [post,comment];
+    
+    
+    
+    
+}
+console.log('welcome to blog post');
+let output=getdata()
+console.log(output);
+
+
+//ex:3
+
+
+
+
