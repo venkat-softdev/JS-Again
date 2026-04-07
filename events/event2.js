64b0240a38da2a3d4530d36aae3ad3a324af8c65
venkatsoftdev <softdev.venkat@gmail.com>
2026-04-07 23:17:46 +0530

    event2 in js

--- events/event2.js
+++ events/event2.js
@@ -1,19 +1,19 @@
 let body = document.querySelector("body");
 body.style.fontFamily = "poppins";
 
-// document.addEventListener('keydown',keyevent)
-// document.addEventListener('keypress',keyevent);
-// document.addEventListener('keyup',keyevent);
+document.addEventListener('keydown',keyevent)
+document.addEventListener('keypress',keyevent);
+document.addEventListener('keyup',keyevent);
 
-// function keyevent(event) {
-//     const eventtype=event.type;
-//     const keycode=event.code;
-//     const keyname=event.key;
-//     console.log(`event type : ${eventtype}`);
-//     console.log(`event code : ${keycode}`);
-//     console.log(`event name : ${keyname}`);
+function keyevent(event) {
+    const eventtype=event.type;
+    const keycode=event.code;
+    const keyname=event.key;
+    console.log(`event type : ${eventtype}`);
+    console.log(`event code : ${keycode}`);
+    console.log(`event name : ${keyname}`);
 
-// }
+}
 
 let label = document.querySelector("label");
 let input = document.querySelector("input");
