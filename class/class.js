ffd4c0f9acb22405b7185f3c6e2f31ea985ee7ad
venkatsoftdev <softdev.venkat@gmail.com>
2026-04-08 22:35:48 +0530

    class in js

---
+++ class/class.js
@@ -0,0 +1,15 @@
+class Employee{
+    employeename="venkat";
+    
+}
+
+ let employee=new Employee();
+
+ console.log(employee.employeename);
+ employee.employeename="yuvaraj";
+ console.log(employee.employeename);
+ 
+ let emp2=new Employee()
+ emp2.employeename="jignesh"
+ console.log(emp2.employeename);
+ 
